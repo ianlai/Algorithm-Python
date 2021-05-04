78ed7be8c5734526404c85c96b88abe36d2e314f
Ian Lai <ian.explore.world@gmail.com>
2021-05-04 20:47:35 +0900

    Added DFS for __0102__Binary Tree Level Order Traversal.py

--- algo/BFS/__0102__Binary Tree Level Order Traversal.py
+++ algo/BFS/__0102__Binary Tree Level Order Traversal.py
@@ -6,9 +6,11 @@
 #         self.right = right
 from collections import deque
 
-# BFS (79%)
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    
+    # BFS [O(n), 79%]
+    def levelOrder1(self, root: TreeNode) -> List[List[int]]:
+        print("BFS")
         if not root:
             return []
         
@@ -25,5 +27,37 @@ class Solution:
                 if cur.right:
                     deq.append(cur.right)
         return ans
+    
+    # ===============================================
+    
+    # DFS [?, 6%] 
+    # Search with DFS by "layer times" 
+    # The targetLevel increments by 1 for each DFS 
+    # Time = 1 + (1 + 2) + (1 + 2 + 4) + (1 + 2 + 4 + 8) 
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        print("DFS")
+        if not root:
+            return []
+        
+        targetLevel = 0
+        ans = []
+        while True:
+            layer = []
+            self.dfs(targetLevel, 0, layer, root)
+            if layer:
+                ans.append(layer)
+            else:
+                break 
+            targetLevel += 1
+        return ans
+    
+    def dfs(self, targetLevel, level, layer, node): 
+        if not node:
+            return 
+        if level == targetLevel:
+            layer.append(node.val)
+            return 
+        self.dfs(targetLevel, level + 1, layer, node.left)
+        self.dfs(targetLevel, level + 1, layer, node.right)
                 
             
