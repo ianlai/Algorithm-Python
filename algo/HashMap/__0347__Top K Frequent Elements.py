98a476016975057d946d7b1ebedb9d4cd5faa5a6
Ian Lai <ian.explore.world@gmail.com>
2021-05-10 10:27:42 +0900

    Use heap to solve __0347__Top K Frequent Elements.py

--- algo/HashMap/__0347__Top K Frequent Elements.py
+++ algo/HashMap/__0347__Top K Frequent Elements.py
@@ -1,7 +1,35 @@
 class Solution:
     
-    #Hashmap [O(nlogn), 67%]
+    #Hashmap + Heap [O(klogn), 67%]
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        print("Heap")
+        if not nums:
+            return []
+        
+        #Count 
+        numTocount = {}
+        for e in nums:
+            if e in numTocount:
+                numTocount[e] += 1
+            else:
+                numTocount[e] = 1     
+                
+        #Store to tuples into heap (maxheap)
+        countToNum = []
+        #heapq.heapify(countToNum)
+        for key in numTocount.keys():
+            heapq.heappush(countToNum, (-numTocount[key], key))
+        
+        #Heappop k items 
+        ans = []
+        for _ in range(k):
+            cur = heapq.heappop(countToNum)[1]
+            ans.append(cur)
+        return ans
+    
+    #Hashmap + Sorting [O(nlogn), 67%]
+    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
+        print("Sorting")
         if not nums:
             return []
         
