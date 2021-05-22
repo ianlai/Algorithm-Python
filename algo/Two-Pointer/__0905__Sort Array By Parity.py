a28bf239296489fefcdea8ac60ca20acaa5e0a28
Ian Lai <ian.explore.world@gmail.com>
2021-05-22 22:16:12 +0900

    Added __0905__Sort Array By Parity.py (partition)

---
+++ algo/Two-Pointer/__0905__Sort Array By Parity.py
@@ -0,0 +1,38 @@
+class Solution:
+    
+    # Partition [Time: O(n) 80% / Space: O(1) 93% ]
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        print("Partition")
+        if not nums:
+            return []
+        left, right = 0, len(nums) - 1 
+        while left <= right: 
+            while left <= right and nums[left] % 2 == 0:
+                left += 1
+            while left <= right and nums[right] % 2 == 1:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        return nums
+    
+    # ==================================================
+    
+    # Two List [Time: O(n) 58% / Space: O(n) 55% ]
+    def sortArrayByParity1(self, nums: List[int]) -> List[int]:
+        print("Two List")
+        if not nums:
+            return []
+        
+        oddList = []
+        evenList = []
+        
+        for num in nums:
+            if num % 2 == 0:
+                evenList.append(num)
+            else:
+                oddList.append(num)
+                
+        evenList.extend(oddList)
+        return evenList
