93f279c84f9fb7ba92c66221c0c16e40f9977a6e
Ian Lai <ian.explore.world@gmail.com>
2021-05-27 02:13:57 +0900

    Added BFS for __0695__Max Area of Island.py

--- algo/DFS/__0695__Max Area of Island.py
+++ algo/DFS/__0695__Max Area of Island.py
@@ -1,7 +1,49 @@
 class Solution:
     
-    # DFS [O(n2), 30%]
+    # BFS [O(n2), 74%]
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        print("BFS")
+        if not grid:
+            return 0
+    
+        maxSize = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    maxSize = max(maxSize, self.bfs(grid, i, j))
+        return maxSize
+    
+    def bfs(self, grid, i, j):
+        deq = collections.deque([(i, j)])
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        size = 1 
+        grid[i][j] = 0 
+        while deq:
+            #print("---")
+            for _ in range(len(deq)):
+                cur = deq.popleft()
+                #grid[cur[0]][cur[1]] = 0 
+                #print(cur)
+                for d in dirs:
+                    nextI = cur[0] + d[0]
+                    nextJ = cur[1] + d[1]
+                    if nextI < 0 or nextI >= len(grid):
+                        continue
+                    if nextJ < 0 or nextJ >= len(grid[0]):
+                        continue
+                    if grid[nextI][nextJ] == 0:
+                        continue
+                    grid[nextI][nextJ] = 0  #Must set 0 before adding to deque, not after taking it out
+                    deq.append((nextI, nextJ))
+                    size += 1
+        #print(i, j, size)
+        return size
+        
+    # ======================================================
+    
+    # DFS, change the island to sea after visited [O(n2), 30% / Worst case: O(n2), 56%]
+    def maxAreaOfIsland1(self, grid: List[List[int]]) -> int:
+        print("DFS")
         if not grid:
             return 0
         
