65d7865053bff9686859dfdf2883b343355130a8
Ian Lai <ian.explore.world@gmail.com>
2021-06-21 23:25:53 +0900

    Added __0547__Number of Provinces.py (dfs)

--- algo/Graph/__0547__Number of Provinces.py
+++ algo/Graph/__0547__Number of Provinces.py
@@ -1,7 +1,54 @@
+from collections import defaultdict
+
 class Solution:
     
-    # Union Find [O(n3):29%]
+    # DFS [O(n):82%]
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        print("DFS")
+        if not isConnected: 
+            return 0
+        
+        nodeMap = self.parse(isConnected)
+        count = self.getCountByTraversal(nodeMap)
+        return count
+    
+    def parse(self, matrix):
+        nodeMap = defaultdict(list)  #node -> [nodes]
+        #nodeMap = {}
+        for i in range(len(matrix)):
+            nodeMap[i] = []
+            for j in range(len(matrix[0])):
+                if i == j:
+                    continue
+                if matrix[i][j] == 0:
+                    continue
+                nodeMap[i].append(j)
+                #nodeMap[j].append(i)
+        return nodeMap
+    
+    def getCountByTraversal(self, nodeMap):
+        visited = set([])
+        count = 0
+        for node in nodeMap:
+            if node in visited:
+                continue
+            count += 1
+            self.dfs(nodeMap, node, visited)
+        return count
+    
+    def dfs(self, nodeMap, node, visited):
+        # if node in visited:
+        #     return
+        for nextNode in nodeMap[node]:
+            if nextNode in visited:
+                continue
+            visited.add(nextNode)
+            self.dfs(nodeMap, nextNode, visited)
+    
+    # ====================================================
+    # Union Find [O(n3):29%]
+    def findCircleNum1(self, isConnected: List[List[int]]) -> int:
+        print("Union Find")
         if not isConnected:
             return 0
         
@@ -16,7 +63,8 @@ class Solution:
                 if isConnected[i][j] == 0:
                     continue
                 # connected case
-                self.union(parents, i, j)
+                if i < j:  #Speed up - 1 (one direction)  [40%]
+                    self.union(parents, i, j)
         return self.count
     
     def union(self, parents, A, B):
