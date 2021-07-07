db8e6960ae701cf8b69baf6045f483e9124a895b
Ian Lai <ian.explore.world@gmail.com>
2021-07-07 20:44:44 +0900

    Updated __0721__Accounts Merge.py

--- algo/UnionFind/__0721__Accounts Merge.py
+++ algo/UnionFind/__0721__Accounts Merge.py
@@ -1,5 +1,72 @@
-class Solution:                            
+class Solution:                      
+    
+    # Union-Find [O(nlogn): 20%]
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        print("2021/07/06")
+        if not accounts:
+            return []
+        
+        idxToEmail = []
+        emailToIdx = collections.defaultdict(int)
+        emailToName = collections.defaultdict(str)
+        
+        # Step1: Parse 
+        idx = 0
+        for data in accounts:
+            name, emails = data[0], data[1:]
+            for email in emails:
+                if email in emailToIdx:
+                    continue
+                idxToEmail.append(email)
+                emailToIdx[email] = idx
+                idx += 1
+                emailToName[email] = name
+                
+        #print(idxToEmail)
+        #print(emailToIdx)
+        #print(emailToName)
+        
+        # Step2: Union (with idx)
+        parent = [-1] * len(idxToEmail)
+        for data in accounts:
+            name, emails = data[0], data[1:]
+            for email in emails[1:]:
+                self.union(parent, emailToIdx[emails[0]], emailToIdx[email])
+    
+        # Step3: Create nameToEmails 
+        idxToEmails = collections.defaultdict(list)
+        for i in range(len(parent)):
+            head = self.find(parent, i)
+            email = idxToEmail[i] #not head
+            idxToEmails[head].append(email)
+        #print(idxToEmails)
+        
+        # Step4: Create results 
+        results = []
+        for idx, emails in idxToEmails.items():
+            result = [emailToName[idxToEmail[idx]]]
+            result.extend(sorted(emails))
+            results.append(result)
+        return results
+    
+    def find(self, parent, e):
+        if parent[e] == -1:
+            return e
+        else:
+            return self.find(parent, parent[e]) 
+            
+    def union(self, parent, e1, e2):
+        if e1 == e2:
+            return 
+        head1 = self.find(parent, e1)
+        head2 = self.find(parent, e2)
+        if head1 != head2:
+            parent[head2] = head1
+                    
+    # ==========================================              
+                    
+    # Union-Find [O(nlogn): 18%]
+    def accountsMerge1(self, accounts: List[List[str]]) -> List[List[str]]:
         if not accounts:
             return []
         
@@ -19,8 +86,8 @@ class Solution:
                 emailToIdx[email] = idx
                 emailToName[email] = name
                 idx += 1
-        #print(emailList)
-        #print(emailToIdx)
+        print(emailList)
+        print(emailToIdx)
 
         
         #union
