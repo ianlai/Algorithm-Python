ee9ce38d6b1b6f6c2acaaa9161f4c79dbbe1038d
Ian Lai <ian.explore.world@gmail.com>
2021-08-03 23:17:37 +0900

    Practiced __0077__Combinations.py

--- algo/DFS/__0077__Combinations.py
+++ algo/DFS/__0077__Combinations.py
@@ -1,5 +1,23 @@
 class Solution:
     
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n <= 0 or k <= 0:
+            return []
+        
+        res = [] 
+        self.dfs(n, k, 0, [], res)
+        return res
+    
+    def dfs(self, n, k, idx, cur, res):
+        if len(cur) == k :
+            res.append(cur)
+            return 
+        
+        for i in range(idx+1, n+1):
+            self.dfs(n, k, i, cur + [i], res)
+
+    # ========================================  
+
     # Backtracking [30%]
     def combine(self, n: int, k: int) -> List[List[int]]:
         if n <= 0 or k <= 0:
