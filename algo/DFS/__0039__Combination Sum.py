6d96365b80197be725a124d64da7f767ddf8567b
Ian Lai <ian.explore.world@gmail.com>
2021-08-03 23:17:59 +0900

    Practiced __0039__Combination Sum.py

--- algo/DFS/__0039__Combination Sum.py
+++ algo/DFS/__0039__Combination Sum.py
@@ -1,7 +1,29 @@
 class Solution:
 
-    # Backtracking [56%]
+    # Backtracking [O(n^(target/min)): 63%]
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if not candidates or target <= 0:
+            return []
+        
+        res = []
+        self.backtracking(candidates, target, 0, [], res)
+        return res
+    
+    def backtracking(self, candidates, target, idx, cur, res):
+        if target == 0:
+            res.append(cur)
+            return
+        if target < 0:
+            return 
+            
+        for i in range(idx, len(candidates)):
+            chosen = candidates[i]
+            self.backtracking(candidates, target - chosen, i, cur + [chosen], res)
+    
+    # ==============================================================
+    
+    # Backtracking [48%]
+    def combinationSum1(self, candidates: List[int], target: int) -> List[List[int]]:
         if candidates is None or len(candidates) == 0:
             return []
         
@@ -15,5 +37,6 @@ class Solution:
             return 
         if target < 0: 
             return 
+        print(cur, target)
         for i in range(idx, len(candidates)):
             self.dfs(candidates, target - candidates[i], i, cur + [candidates[i]], ans)
