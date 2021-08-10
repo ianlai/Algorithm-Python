5b8c3339d6dc4112bcbfe19096fefa58e6a7b25b
Ian Lai <ian.explore.world@gmail.com>
2021-08-10 14:30:32 +0900

    Updated __1130__Minimum Cost Tree From Leaf Values.py

--- algo/DP/__1130__Minimum Cost Tree From Leaf Values.py
+++ algo/DP/__1130__Minimum Cost Tree From Leaf Values.py
@@ -2,10 +2,10 @@ class Solution:
     
     # DP [O(n3): 15%]
     def mctFromLeafValues(self, arr: List[int]) -> int:
+        
         m = len(arr)
         dp = [[0 for _ in range(m)] for _ in range(m)]
 
-                
         for d in range(1, m):
             for i in range(m):
                 if i+d >= m:
@@ -20,13 +20,9 @@ class Solution:
                 dp[i][i+d] = minVal
                 
                 ### Wrong (compare only left and right two possibilities)
-                left  = dp[i][i+d-1] + max(arr[i:i+d]) * arr[i+d]
-                right = dp[i+1][i+d] + max(arr[i+1:i+d+1]) * arr[i]
+                #left  = dp[i][i+d-1] + max(arr[i:i+d]) * arr[i+d]
+                #right = dp[i+1][i+d] + max(arr[i+1:i+d+1]) * arr[i]
                 #dp[i][i+d] = min(left, right)
                 #print(i,",",i+d,":",left, right, minVal)
-                
-        
-        # for i in range(m):
-        #     print(dp[i])
         
         return dp[0][m-1]
