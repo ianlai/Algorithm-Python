cc00a7046ebe67b3dcb125ce89ae325019d16b5b
Ian Lai <ian.explore.world@gmail.com>
2021-08-30 00:41:50 +0900

    Updated __0538__Convert BST to Greater Tree.py

--- algo/Tree/__0538__Convert BST to Greater Tree.py
+++ algo/Tree/__0538__Convert BST to Greater Tree.py
@@ -5,22 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    
+    # DESC traversal [O(n): 58%]
     def convertBST(self, root: TreeNode) -> TreeNode:
+        print("DESC traversal")
+        self.largerSum = 0
+        self.dfs(root)
+        return root
+    
+    def dfs(self, root):
+        if not root:
+            return 
+        self.dfs(root.right)
+        temp = root.val 
+        root.val += self.largerSum
+        self.largerSum += temp 
+        self.dfs(root.left)
+        
+    # ========================================
+    
+    # Two-step divide and conquer [O(n): 58%]  
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        print("Two step ")
         if not root:
             return root
         
         self.helper1(root)
         self.helper2(root, 0)
         return root
-        
+    
+    # add the right subtree 
     def helper1(self, root):
         if not root:
             return 0
         r = self.helper1(root.right)
-        root.val += r 
-        l = self.helper1(root.left)
-        return root.val + l
+        root.val += r        #root's val becomes sum of root and sum of root's right (root + right) 
+        l = self.helper1(root.left) 
+        return root.val + l  #return val is sum of the root's subtree (left + root+ right)
     
+    # add the right parent
     def helper2(self, root, parentVal):
         if not root:
             return 
@@ -28,4 +51,3 @@ class Solution:
         self.helper2(root.left, root.val)
         self.helper2(root.right, parentVal)
         return 
-        
