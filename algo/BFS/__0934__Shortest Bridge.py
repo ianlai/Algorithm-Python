68e9ae44293995431236d287abe5ed3192e8843b
Ian Lai <ian.explore.world@gmail.com>
2021-09-06 01:55:53 +0900

    Updated __0934__Shortest Bridge.py with one-time BFS

--- algo/BFS/__0934__Shortest Bridge.py
+++ algo/BFS/__0934__Shortest Bridge.py
@@ -1,19 +1,67 @@
-DEBUG = False
+DEBUG = True
 class Solution:
     
-    # DFS to chagne island1 ; BFS to find min path to island2 [5%]
+    # BFS starting with island2 [O(mn):62%] 
     def shortestBridge(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0        
+        m, n = len(grid), len(grid[0])
+        
+        # convert the first island to "2" (island2); the second island stays as "1" 
+        def convertFirstIsland(grid):
+            island2 = []
+            for i in range(m):
+                for j in range(n):
+                    if grid[i][j] == 1:
+                        _dfsChange(grid, island2, i, j)
+                        return island2
+                        
+        def _dfsChange(grid, island2, i, j):
+            if not (0 <= i < m and 0 <= j < n):
+                return
+            if grid[i][j] != 1: # 0 or 2
+                return 
+            grid[i][j] = 2
+            island2.append((i, j))
+            for ni, nj in [(i+1, j),(i-1, j),(i, j+1),(i, j-1)]:
+                _dfsChange(grid, island2, ni, nj)
+        
+        def findMinDistance(grid, island2):
+            visited = set(island2)
+            deq = collections.deque(island2)
+            distance = -1
+            while deq:
+                for _ in range(len(deq)):
+                    i, j = deq.popleft()
+                    if grid[i][j] == 1:  #found island1
+                        return distance
+                    for ni, nj in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                        if not (0 <= ni < m and 0 <= nj < n):
+                            continue
+                        if (ni, nj) in visited:
+                            continue
+                        deq.append((ni, nj))
+                        visited.add((ni, nj))
+                distance += 1
+        
+        island2 = convertFirstIsland(grid)
+        distance = findMinDistance(grid, island2)
+        return distance
+        
+    # =============================================================
+    
+    # DFS to chagne island1 ; BFS to find min path to island2 [5%]
+    def shortestBridge1(self, grid: List[List[int]]) -> int:
+        print("1")
         if not grid or not grid[0]:
             return 0
         self.minFlip = float('inf')
         
-        self.printGrid(grid)
+        self.printGrid(grid, "original")
         self.convertFirstIsland(grid)
         
-        self.printGrid(grid)
+        self.printGrid(grid, "converted")
         self.findMinFromFirstIsland(grid)
-        
-        self.printGrid(grid)
         
         return self.minFlip
     
@@ -21,37 +69,34 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    self.dfsChange(grid, i, j) #change to (2)
+                    self._dfsConvert(grid, i, j) #change to (2)
                     return 
                 
-    def dfsChange(self, grid, i, j):
+    def _dfsConvert(self, grid, i, j):
         m, n = len(grid), len(grid[0])
         if 0 <= i < m and 0 <= j < n:
             if grid[i][j] == 1:
                 grid[i][j] = 2
                 for ni, nj in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
-                    self.dfsChange(grid, ni, nj)
+                    self._dfsConvert(grid, ni, nj)
                     
     def findMinFromFirstIsland(self, grid):
         m, n = len(grid), len(grid[0])
-        dis = [[float('inf') for _ in range(n)] for _ in range(m)]
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     minDis = float('inf')
-                    
                     for ni, nj in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
-                        #self.dfsFind(grid, ni, nj, 0, dis) #find island2 (1) 
                         if not (0 <= ni < m and 0 <= nj < n):
                             continue
-                        minDis = min(minDis, self.bfsFind(grid, ni, nj)) #find island2 (1)
-                        
-                    dis[i][j] = minDis
+                        minDis = min(minDis, self._bfsFind(grid, ni, nj)) #find island2 (1)
+                    minDis = min(minDis, self._bfsFind(grid, i, j)) #find island2 (1)
+
                     self.minFlip = min(self.minFlip, minDis)
                     
     # OK
-    def bfsFind(self, grid, row, col):
+    def _bfsFind(self, grid, row, col):
         m, n = len(grid), len(grid[0])
         deq = collections.deque([(row, col)])
         distance = {(row, col) : 0}
@@ -73,19 +118,17 @@ class Solution:
                     continue
                 distance[(ni, nj)] = distance[(i, j)] + 1
                 deq.append((ni, nj))
-        return float('inf')    
+        return float('inf')
             
-    # Failed
-    def dfsFind(self, grid, i, j, distance, dis):
+    # DFS (Failed)
+    def _dfsFind(self, grid, i, j, distance, dis):
         m, n = len(grid), len(grid[0])
         if 0 <= i < m and 0 <= j < n:
 
             if grid[i][j] == 2: #island-1
                 return 
-        
             if grid[i][j] == 0: #sea
                 distance += 1
-                
                 if distance > dis[i][j]:
                     return 
                 else:
@@ -93,17 +136,17 @@ class Solution:
 
                 #grid[i][j] = 3 #visited
                 for ni, nj in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
-                    self.dfsFind(grid, ni, nj, distance, dis)
+                    self._dfsFind(grid, ni, nj, distance, dis)
                 #grid[i][j] = 0 #backtracking 
 
             if grid[i][j] == 1: #island-2
                 self.minFlip = min(self.minFlip, distance)
                 return 
             
-    def printGrid(self, grid):
+    def printGrid(self, grid, msg):
         if not DEBUG:
             return 
-        print("----------")
+        print("-----" + msg + "-----")
         for row in grid:
             print(row)
             
