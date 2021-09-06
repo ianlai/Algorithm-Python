57248b1dc81301f0db19e8d31bf54ddaa3b6b59a
Ian Lai <ian.explore.world@gmail.com>
2021-09-06 22:24:25 +0900

    Improved __0958__Check Completeness of a Binary Tree.py

--- algo/Tree/__0958__Check Completeness of a Binary Tree.py
+++ algo/Tree/__0958__Check Completeness of a Binary Tree.py
@@ -5,14 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    
+    # BFS + it's complete tree if any not-none after a None [O(n): 95%]
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        print("BFS + it's complete tree if any not-none after a None")
+        deq = collections.deque([root])
+        while deq:
+            cur = deq.popleft()
+            if not cur:
+                if any(deq):
+                    return False
+                return True
+            deq.append(cur.left)
+            deq.append(cur.right)
+        return True #will not fall into this (must encounter a None before completing the BFS)
+            
+    # ====================================================
+    # BFS + calculate by index [O(n): 29%]
+    def isCompleteTree1(self, root: Optional[TreeNode]) -> bool:
+        print("BFS + calculate by index")
         if not root:
             return True
         
         deq = collections.deque([(root, 0)])
         layer = -1
         while deq:
-            
             lastIdx = -1
             lastLayer = True
             layer += 1 
@@ -20,6 +37,7 @@ class Solution:
             
             for _ in range(numOfLayer):
                 cur, idx = deq.popleft()
+                
                 if cur.left:
                     deq.append((cur.left, idx*2))
                     lastLayer = False
