376058b3d07412d23a95dca1ae953294e4392711
Ian Lai <ian.explore.world@gmail.com>
2021-10-04 14:39:42 +0900

    Updated __0322__Coin Change.py for Bottom-Up DP

--- algo/DP/__0322__Coin Change.py
+++ algo/DP/__0322__Coin Change.py
@@ -1,7 +1,63 @@
+MAX_VAL = float('inf')
+
 class Solution:
-    
-    # DP (memoization), with return value [10%] 
+
+    # Buttom-Up DP [Time: 23% / Space:22%]
     def coinChange(self, coins: List[int], amount: int) -> int:
+        print("Bottom-Up DP")
+        
+        # Initialize
+        dp = [[inf for _ in range(amount+1)] for _ in range(len(coins))]
+        for row in dp:
+            row[0] = 0
+            
+        # DP loop
+        for i, coin in enumerate(coins):
+            for j in range(amount+1):
+                if j < coin:
+                    dp[i][j] = dp[i-1][j]
+                    continue
+                if i == 0 and j - coin >= 0:
+                    dp[i][j] = dp[i][j-coin] + 1
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-coin] + 1) 
+
+        return dp[-1][-1] if dp[-1][-1] != inf else -1
+    
+    # =====================================================
+    
+    # Top-Down DP [Time: 5% / Space: 5%]
+    def coinChange1(self, coins: List[int], amount: int) -> int:
+        print("Top-Down DP")
+        def dfs(coins, amount, idx, cur, minNum, memo):
+            if amount < 0:
+                return MAX_VAL
+            if amount == 0:
+                #return min(len(cur), minNum) #WRONG
+                return 0
+            if amount in memo:
+                return memo[amount]
+            
+            localMin = MAX_VAL
+            #for i in range(0, len(coins)):  #WRONG
+            for i in range(0, len(coins)):
+                localMin = min(localMin, 1 + dfs(coins, amount - coins[i], i, cur + [coins[i]], minNum, memo))
+            memo[amount] = localMin
+            return memo[amount] 
+            
+        if len(coins) == 0 and amount == 0:
+            return 0
+        if len(coins) == 0 and amount != 0:
+            return -1
+        memo = {}
+        amountList = [MAX_VAL] * (amount + 1)
+        minNum = dfs(coins, amount, 0, [], MAX_VAL, memo)
+        return minNum if minNum != MAX_VAL else -1
+    
+    #=====================================================
+
+    # DP (memoization), with return value [10%] 
+    def coinChange1(self, coins: List[int], amount: int) -> int:
         if coins is None or len(coins) == 0:
             return -1
         
@@ -28,40 +84,6 @@ class Solution:
         memo[amount] = result
         return result
         
-    
-      # DP (memoization), with answer array 
-#     def coinChange(self, coins: List[int], amount: int) -> int:
-#         if coins is None or len(coins) == 0:
-#             return -1
-        
-#         ans = [float('inf'), float('inf')]
-#         memo = {}
-#         self.dfs(coins, amount, 0, [], ans, memo)
-        
-#         #print(memo)
-#         #print(ans)
-        
-#         return -1 if ans[0] == float('inf') else ans[0]
-        
-        
-#     def dfs(self, coins, amount, idx, cur, ans, memo):
-#         #memoization
-#         if amount < 0:
-#             ans[1] = memo[amount] = -1
-#             return 
-        
-#         if amount in memo:
-#             ans[1] = memo[amount]
-#             return 
-
-#         if amount == 0:
-#             ans[0] = min(ans[0], len(cur))
-#             return
-        
-#         for i in range(idx, len(coins)):
-#             self.dfs(coins, amount - coins[i], i, cur + [coins[i]], ans, memo)
-#         memo[amount] = ans[1]
-        
     #=======================================================================
     
     #DFS: TLE 
@@ -83,4 +105,3 @@ class Solution:
             return 
         for i in range(idx, len(coins)):
             self.dfs1(coins, amount - coins[i], i, cur + [coins[i]], ans)
-        
