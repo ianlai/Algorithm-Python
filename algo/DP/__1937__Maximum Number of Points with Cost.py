34810f1b8301282b5a59775dca94cbcec1999993
Ian Lai <ian.explore.world@gmail.com>
2021-10-06 10:05:43 +0900

    Updated __1937__Maximum Number of Points with Cost.py with 1 dp table

--- algo/DP/__1937__Maximum Number of Points with Cost.py
+++ algo/DP/__1937__Maximum Number of Points with Cost.py
@@ -1,6 +1,29 @@
 class Solution:
     
-    #DP [O(n2): 32%] 
+    #DP with 1 DP table [O(n2): 32%] 
+    def maxPoints(self, points: List[List[int]]) -> int:
+        
+        m, n = len(points), len(points[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0] = points[0]
+        
+        for i in range(1, m):
+            #Left 
+            dp[i][0] = points[i][0] + dp[i-1][0]
+            for j in range(1, n):
+                up = points[i][j] + dp[i-1][j]
+                dp[i][j] = max(up, dp[i][j-1] + points[i][j] - points[i][j-1] - 1)      #j-col val vs calculated from left cols 
+            
+            #Right
+            dp[i][n-1] = max(dp[i][n-1], points[i][n-1] + dp[i-1][n-1])
+            for j in range(n-2, -1, -1):
+                dp[i][j] = max(dp[i][j], dp[i][j+1] + points[i][j] - points[i][j+1] - 1) #dp val vs calculated from right cols
+
+        return max(dp[-1])
+    
+    #===============================================
+
+    #DP with 2 DP tables [O(n2): 32%] 
     def maxPoints(self, points: List[List[int]]) -> int:
         
         m, n = len(points), len(points[0])
