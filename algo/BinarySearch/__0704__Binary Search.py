de2ee1b98c1d47af5725621001633b5ad2fda328
Ian Lai <ian.explore.world@gmail.com>
2021-11-05 00:00:08 +0900

    Updated __0704__Binary Search.py

--- algo/BinarySearch/__0704__Binary Search.py
+++ algo/BinarySearch/__0704__Binary Search.py
@@ -1,7 +1,25 @@
 class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        print("close-open")
+    
+        start, end = 0, len(nums)
+        while start < end:
+            mid = start + (end - start) // 2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                end = mid
+            else:
+                start = mid + 1
+        if nums[mid] == target:
+            return mid
+        return -1
+    
+    # ================================================
     
     #Binary Search [O(logn), 39%]
-    def search(self, nums: List[int], target: int) -> int:
+    def search1(self, nums: List[int], target: int) -> int:
+        print("Jiuzhang: open-open")
         if not nums:
             return -1
         
@@ -19,4 +37,3 @@ class Solution:
         if target == nums[end]:
             return end
         return -1
-            
