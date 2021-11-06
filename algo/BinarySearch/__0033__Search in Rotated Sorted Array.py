7c5e3c466120107c332e2c03a6249a1d29776da2
Ian Lai <ian.explore.world@gmail.com>
2021-11-06 18:00:50 +0900

    Updated __0033__Search in Rotated Sorted Array.py, __0153__Find Minimum in Rotated Sorted Array.py

--- algo/BinarySearch/__0033__Search in Rotated Sorted Array.py
+++ algo/BinarySearch/__0033__Search in Rotated Sorted Array.py
@@ -1,5 +1,39 @@
 class Solution:
+
     
+    # Close-Open Binary Search [O(logn): 81%]
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        # Step1: Find min
+        start, end = 0, len(nums)
+        last = nums[-1]
+        while start < end:
+            mid = start + (end - start) // 2
+            if nums[mid] <= last:
+                end = mid
+            else:
+                start = mid + 1
+        minVal, minIdx = nums[start], start
+        print("minIdx:", minIdx, "minVal:", minVal)
+        
+        # Step2: Find target in left or right intervals
+        if target <= last:  #right interval 
+            start, end = minIdx, len(nums)
+        else:
+            start, end = 0, minIdx #[0, minIdx-1] 
+        while start < end: 
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1 
+            else:
+                end = mid 
+        return -1 
+    
+    # ===================================================
     # Binary Search Twice (min -> target) [O(logn), 69%]
     def search(self, nums: List[int], target: int) -> int:
         if not nums:
--- algo/BinarySearch/__0153__Find Minimum in Rotated Sorted Array.py
+++ algo/BinarySearch/__0153__Find Minimum in Rotated Sorted Array.py
@@ -1,17 +1,53 @@
 class Solution:
     
-    # Binary Search [O(logn), 96%]
+    # Close-open interval
     def findMin(self, nums: List[int]) -> int:
+        print("Version-3")
+        if not nums: 
+            return -1
+        
+        start, end = 0, len(nums)
+        target = nums[-1]
+        while start < end:
+            mid = start + (end - start) // 2
+            if nums[mid] <= target:
+                end = mid
+            else:
+                start = mid + 1
+        return nums[start]
+    
+    # =========================================
+    # Close-close interval (Jiuzhang)
+    def findMin2(self, nums: List[int]) -> int:
+        print("Version-2")
+        if not nums:
+            return -1
+        
+        start, end = 0, len(nums) - 1
+        target = nums[-1]
+        while start + 1 < end:
+            mid = start + (end - start) // 2 
+            if nums[mid] <= target: 
+                end = mid
+            else:                   
+                start = mid
+                
+        return min(nums[start], nums[end]) #no special case judgement
+    
+    # =========================================
+    # Binary Search [O(logn), 96%]
+    def findMin1(self, nums: List[int]) -> int:
         if not nums:
             return -1
         
         start, end = 0, len(nums) - 1
         
+        target = nums[0]
         while start + 1 < end:
             mid = start + (end - start) // 2 
-            if nums[mid] > nums[0]:
+            if nums[mid] > target: # >= first (left xxx) 
                 start = mid
-            else:
+            else:                   # < first (right ooo) , but asc order can't be found
                 end = mid
                 
-        return min(nums[0], min(nums[start], nums[end])) #asc is special case
+        return min(nums[0], min(nums[start], nums[end])) #asc is special case 
