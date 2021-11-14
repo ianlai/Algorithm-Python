b8fdbde156567294fa661d1a638982dc3ddc123d
Ian Lai <ian.explore.world@gmail.com>
2021-11-14 21:14:06 +0900

    Updated __0207__Course Schedule.py

--- algo/BFS/__0207__Course Schedule.py
+++ algo/BFS/__0207__Course Schedule.py
@@ -1,8 +1,47 @@
 class Solution:
     
-    #Topologic sorting [O(n), 5%]
-    #Note: Jiuzhang defines graph (vertex); while Leetcode defines the prerequisites (edge)
+    # 2021/11/14
+    # BFS (Topological sorting) [O(n): 97%]
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        print("Code-2")
+        if not prerequisites or not numCourses:
+            return True
+        nToNext = self.buildMap(numCourses, prerequisites)
+        starts = [n for n in nToNext if nToNext[n][0] == 0]
+        numDone = len(starts)
+        
+        #print(nToNext)
+        #print("starts:", starts)
+        deq = collections.deque(starts)
+        while deq:
+            cur = deq.popleft()
+            for nxt in nToNext[cur][1]:
+                nToNext[nxt][0] -= 1 
+                if nToNext[nxt][0] == 0:
+                    deq.append(nxt)
+                    numDone += 1
+                    
+        #print(nToNext)   
+        if numDone == numCourses:
+            return True
+        return False
+            
+    def buildMap(self, numCourses, prerequisites):
+        nToNext = collections.defaultdict(list)
+        for n in range(numCourses):
+            nToNext[n] = [0, []]
+        for nxt, cur in prerequisites:
+            nToNext[nxt][0] += 1        #indegree
+            nToNext[cur][1].append(nxt) #out course
+        return nToNext
+            
+    #=================================================
+        
+    # 2021/05/09
+    # Topological sorting [O(n), 5%]
+    # Note: Jiuzhang defines graph (vertex); while Leetcode defines the prerequisites (edge)
+    def canFinish1(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        print("Code-1")
         if not prerequisites or not numCourses:
             return True
         
