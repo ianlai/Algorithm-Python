8fe1cb2bd8ac64c6bb5abc6d8f1496958e788ee4
Ian Lai <ian.explore.world@gmail.com>
2021-11-14 18:32:18 +0900

    Updated __0718__Maximum Length of Repeated Subarray.py

--- algo/DP/__0718__Maximum Length of Repeated Subarray.py
+++ algo/DP/__0718__Maximum Length of Repeated Subarray.py
@@ -1,7 +1,53 @@
 class Solution:
     
-    # DP [11%]
+    # DP (2D array) [O(n2): 82%]
+    def findLength(self, A, B):
+        print("Method3: 2D array 正著走; 全部的點都走過")
+        memo = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        count = 0
+        for i in range(len(A) - 1, -1, -1):
+            for j in range(len(B) - 1, -1, -1):
+                count += 1 
+                print(count)
+                if A[i] == B[j]:
+                    memo[i][j] = memo[i + 1][j + 1] + 1
+        return max(max(row) for row in memo)
+    
+    #=======================================
+    
+    # DP (2D array) [O(n2): 8%]
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        print("Method2: 2D array 倒著走; 只走有相同的點")
+        
+        # Preprocess to record the same values in two arrays
+        v1ToI2 = collections.defaultdict(list)
+        for i1, v1 in enumerate(nums1):
+            if v1 in v1ToI2:
+                continue
+            for i2, v2 in enumerate(nums2):
+                if v1 == v2:
+                    v1ToI2[v2].append(i2)
+        #print(v1ToI2)
+        
+        res = 0
+        maxLength = [[0 for _ in range(len(nums2))] for _ in range(len(nums1))]
+        for i1, v1 in enumerate(nums1):
+            if v1 in v1ToI2:
+                for i2 in v1ToI2[v1]:  #Only check the pair who matches in two arrays
+                    maxLength[i1][i2] = 1
+                    if i1 == 0: 
+                        res = max(res, maxLength[i1][i2])
+                        continue 
+                    if 0 <= i2 - 1 < len(nums2) and nums2[i2 - 1] == nums1[i1 - 1]:
+                        maxLength[i1][i2] = max(maxLength[i1][i2], maxLength[i1-1][i2-1] + 1)
+                    res = max(res, maxLength[i1][i2])
+        return res
+    
+    #=======================================
+
+    # DP (2D map) [8%]
+    def findLength1(self, nums1: List[int], nums2: List[int]) -> int:
+        print("Method1: 2D map")
         
         v1ToI2 = collections.defaultdict(list)
         for i1, v1 in enumerate(nums1):
@@ -34,7 +80,7 @@ class Solution:
             #print(k, ":", v)
         return res
     
-    # =======================================
+    #=======================================
     
     #Incorrect, element might be more than 1 digit
     def findLength1(self, nums1: List[int], nums2: List[int]) -> int:
