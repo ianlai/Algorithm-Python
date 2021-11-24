af3747ecdf3590b209549d8919ba4410fa91a733
Ian Lai <ian.explore.world@gmail.com>
2021-11-24 23:43:52 +0900

    Updated __0133__Clone Graph.py

--- algo/BFS/__0133__Clone Graph.py
+++ algo/BFS/__0133__Clone Graph.py
@@ -7,6 +7,49 @@ class Node:
 """
 
 class Solution:
+    
+    # 2021/11/24
+    # BFS
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node is None:
+            return None
+        
+        deq = collections.deque([node])
+        visited = set([node])
+        curToNew = {}
+        
+        #BFS to clone node and mapping
+        while deq:
+            curNode = deq.popleft()
+            
+            newNode = Node(curNode.val) #node
+            curToNew[curNode] = newNode #mapping
+            
+            for neighbor in curNode.neighbors:
+                if neighbor in visited:
+                    continue
+                visited.add(neighbor)
+                deq.append(neighbor)
+        
+        
+        #BFS again to clone the edges 
+        deq = collections.deque([node])
+        visited = set([node])
+        while deq:
+            curNode = deq.popleft()
+            newNode = curToNew[curNode]
+            for neighbor in curNode.neighbors:
+                newNode.neighbors.append(curToNew[neighbor])
+                if neighbor in visited:
+                    continue
+                visited.add(neighbor)
+                deq.append(neighbor)
+                
+        return curToNew[node]
+    
+    
+    # 2021/05/07 
+    # BFS 
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return node
@@ -25,11 +68,6 @@ class Solution:
                 mydeq.append(neighbor)
                 myset.add(neighbor)
                 oldToNew[neighbor] = Node(neighbor.val)  #add old to new mapping
-        
-        # Debug
-        # for old in oldToNew.keys():
-        #     new = oldToNew[old]
-        #     print(old.val, id(old), "--", new.val, id(new))
         
         # Add edge mappings between old nodes and new nodes
         mylist = list(myset)
