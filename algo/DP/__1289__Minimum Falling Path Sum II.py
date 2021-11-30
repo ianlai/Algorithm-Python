30bb565523cd4941ddd643087e7bf82536dc3f5c
Ian Lai <ian.explore.world@gmail.com>
2021-11-30 21:52:09 +0900

    Updated __1289__Minimum Falling Path Sum II.py

--- algo/DP/__1289__Minimum Falling Path Sum II.py
+++ algo/DP/__1289__Minimum Falling Path Sum II.py
@@ -1,7 +1,39 @@
 class Solution:
     
-    
+    # DP + Heap [O(n*(nlogn+n)) = O(n2*logn): 38%]
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        print("Code-2: DP + Heap")
+        n = len(matrix)
+        
+        # init 
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        h1 = []
+        for j in range(n):
+            dp[0][j] = matrix[0][j]
+            heapq.heappush(h1, (dp[0][j], j))
+        
+        # dp 
+        for i in range(1, n):
+            h2 = []
+            for j in range(n):
+
+                min1, min1Idx = heapq.heappop(h1)
+                min2, min2Idx = h1[0]
+                heapq.heappush(h1, (min1, min1Idx))
+                if min1Idx != j:
+                    minVal = min1
+                else:
+                    minVal = min2
+                dp[i][j] = minVal + matrix[i][j]
+                heapq.heappush(h2, (dp[i][j], j))
+            h1 = h2
+        return min(dp[n-1])
+    
+    # ==============================================================
+    
+    # DP [O(n3): 25%]
+    def minFallingPathSum1(self, matrix: List[List[int]]) -> int:
+        print("Code-1: DP")
         n = len(matrix)
         
         # init 
