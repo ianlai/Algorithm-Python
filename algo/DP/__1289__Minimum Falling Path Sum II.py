2985ab72f8b98ee9715bbe9780131906dd90ada3
Ian Lai <ian.explore.world@gmail.com>
2021-12-02 00:20:11 +0900

    Updated __1289__Minimum Falling Path Sum II.py with min list

--- algo/DP/__1289__Minimum Falling Path Sum II.py
+++ algo/DP/__1289__Minimum Falling Path Sum II.py
@@ -1,8 +1,56 @@
 class Solution:
     
-    # DP + Record two mins [O(n2): 74%]
+    
+    # DP + Two-size list with general Top-k func [O(n*kn), k=2: 37%]
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        print("Code-3: DP + Two Mins")
+        print("Code-5: DP + Two-size list with general top-k func")
+        n = len(matrix)
+        
+        # Find top-k 
+        def push(arr, k, val, less):
+            if len(arr) == 0:
+                arr.append(val)
+            else:
+                for i in range(len(arr)):
+                    if less(val[1], arr[i][1]):
+                        arr.insert(i, val)
+                        if len(arr) > k:
+                            arr.pop()
+                            return
+                arr.append(val)
+                if len(arr) > k:
+                    arr.pop()
+                #arr = arr[:k]
+        
+        # Init 
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        for j in range(n):
+            dp[0][j] = matrix[0][j]
+        
+        # DP 
+        for i in range(1, n):
+            # Find 1st and 2nd smallest in row i 
+            arr = []
+            for j in range(n): 
+                push(arr, 2, (j, dp[i-1][j]), lambda x, y: x < y)
+                
+            min1Idx, min1Val = arr[0]
+            min2Idx, min2Val = arr[1]
+
+            # Inner loop (N) 
+            for j in range(n):
+                if j == min1Idx:
+                    minVal = min2Val
+                else:
+                    minVal = min1Val
+                dp[i][j] = minVal + matrix[i][j]
+        return min(dp[n-1])
+    
+    # ==============================================================
+    
+    # DP + Two-size queue [O(n2): 54%]
+    def minFallingPathSum4(self, matrix: List[List[int]]) -> int:
+        print("Code-4: DP + Two-size queue ")
         n = len(matrix)
         
         # Init 
@@ -13,6 +61,50 @@ class Solution:
         # DP 
         for i in range(1, n):
             # Find 1st and 2nd smallest in row i 
+            deq = collections.deque()
+            if dp[i-1][0] < dp[i-1][1]:
+                deq.append((0, dp[i-1][0]))
+                deq.append((1, dp[i-1][1]))
+            else:
+                deq.append((1, dp[i-1][1]))
+                deq.append((0, dp[i-1][0]))
+                
+            for j in range(2, n):
+                cur = (j, dp[i-1][j])
+                if cur[1] < deq[0][1]:
+                    deq.pop()
+                    deq.appendleft(cur)
+                elif cur[1] < deq[1][1]:
+                    deq.pop()
+                    deq.append(cur)
+            
+            min2Idx, min2Val = deq.pop()
+            min1Idx, min1Val = deq.pop()
+            
+            # Inner loop (N) 
+            for j in range(n):
+                if j == min1Idx:
+                    minVal = min2Val
+                else:
+                    minVal = min1Val
+                dp[i][j] = minVal + matrix[i][j]
+        return min(dp[n-1])
+    
+    # ==============================================================
+    
+    # DP + Record two mins [O(n2): 74%]
+    def minFallingPathSum3(self, matrix: List[List[int]]) -> int:
+        print("Code-3: DP + Two Mins")
+        n = len(matrix)
+        
+        # Init 
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        for j in range(n):
+            dp[0][j] = matrix[0][j]
+        
+        # DP 
+        for i in range(1, n):
+            # Find 1st and 2nd smallest in row i (3N)
             min1Val = min(dp[i-1])
             min1Idx = dp[i-1].index(min1Val)
             min2Val = inf
@@ -21,7 +113,7 @@ class Solution:
             if dp[i-1][min1Idx+1:]:
                 min2Val = min(min2Val, min(dp[i-1][min1Idx+1:]))
             
-            # Inner loop
+            # Inner loop (1N)
             for j in range(n):
                 minVal = min1Val
                 if j == min1Idx:
