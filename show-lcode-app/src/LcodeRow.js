fc222591b452526e82fb5b4f7558d53bccd7be87
Ian Lai <ian.explore.world@gmail.com>
2021-12-03 01:38:40 +0900

    [React] Added TagButton component

--- show-lcode-app/src/LcodeRow.js
+++ show-lcode-app/src/LcodeRow.js
@@ -1,3 +1,5 @@
+import TagButton from "./TagButton";
+
 function LcodeRow(props) {
   return (
     <tr class="active-row">
@@ -5,7 +7,11 @@ function LcodeRow(props) {
       <th>
           <a href={props.url} class="large-font"> {props.title} </a> 
         </th>
-      <th>{props.tags}</th>
+      <th>{
+      props.tags.map(tag => (
+          <TagButton name={tag}/> 
+      ))
+      }</th>
       <th>{props.memo}</th>
     </tr>
   );
---
+++ show-lcode-app/src/TagButton.js
@@ -0,0 +1,8 @@
+function TagButton(props) {
+    return (
+        <button class = "button-1" role="button"> {props.name} </button>
+    );
+  }
+  
+  export default TagButton;
+  
