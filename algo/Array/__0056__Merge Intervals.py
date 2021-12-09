055b875c974d40eb41f3d62d37f042d29fc39a4a
Ian Lai <ian.explore.world@gmail.com>
2021-12-09 22:09:13 +0900

    Updated __1094__Car Pooling.py, __0056__Merge Intervals.py

--- algo/Array/__0056__Merge Intervals.py
+++ algo/Array/__0056__Merge Intervals.py
@@ -1,5 +1,55 @@
 class Solution:
+        
+    # 2021/12/09 
+    # Timestamp [O(nlogn + n): 92%]
+    def merge3(self, intervals: List[List[int]]) -> List[List[int]]:
+        print("Code-3: timestamp")
+        timestamps = collections.defaultdict(list)
+        for s, e in intervals:
+            timestamps[s].append(1)
+            timestamps[e].append(-1)       
+        
+        res = []
+        openCount = 0
+        start, end = -1, -1
+        for t, onlist in sorted(timestamps.items()): 
+            openCount += sum(onlist)
+            if openCount != 0:
+                if start == -1: #first
+                    start = t 
+            else:
+                end = t
+                if start == -1:  #special case: [end, end]
+                    res.append([end, end])    
+                else:
+                    res.append([start, end])
+                start, end = -1, -1
+        return res
+    
+    # =====================================================
+
+    # 2021/12/09
+    # Compare the adjacent two intervals (no pop) [O(nlogn): 79%]
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        print("Code-2: Compare the adjacent two intervals and put into new list (no pop)")
+        if not intervals:
+            return []
+        
+        intervals.sort(key = lambda x: x[0])
+        res = []
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res
+    
+    # =====================================================
+        
+    # 2021/07/09 
+    # Compare the adjacent two intervals (pop one) [O(n^2): 39%]
+    def merge1(self, intervals: List[List[int]]) -> List[List[int]]:
+        print("Code-1: ")
         if not intervals:
             return []
         
@@ -8,11 +58,8 @@ class Solution:
         while j < len(intervals):
             if intervals[j][0] <= intervals[i][1]:  #merge
                 intervals[i][1] = max(intervals[i][1], intervals[j][1])
-                intervals.pop(j)
+                intervals.pop(j) #O(n) operation
             else:
                 i += 1
                 j += 1
-                
-        results = []
         return intervals
-                
--- algo/Heap/__1094__Car Pooling.py
+++ algo/Heap/__1094__Car Pooling.py
@@ -1,9 +1,28 @@
 #https://leetcode.com/problems/car-pooling/
 class Solution:
     
-    # Heap [O(N+NlogN):44%]
+    # 2021/12/09
+    # Store all timestamps and pop from heap [O(N+NlogN):87%]
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        
+        print("Code-2: Store all timestamps and pop from heap")
+        timestamps = []
+        for trip in trips:
+            heappush(timestamps, (trip[1], trip[0]))
+            heappush(timestamps, (trip[2], -trip[0]))
+        used = 0
+        while timestamps:
+            t = heappop(timestamps)
+            used += t[1]
+            if used > capacity:
+                return False
+        return True
+    
+    # ===============================================
+    
+    # 2021/12/08
+    # Sort by from time, pop end time from heap [O(N+NlogN): 87%]
+    def carPooling1(self, trips: List[List[int]], capacity: int) -> bool:
+        print("Code-1: Sort by from time, pop end time from heap")
         fromMap = collections.defaultdict(list)
         for num, f, t in trips:
             fromMap[f].append([t, num])
@@ -21,8 +40,7 @@ class Solution:
             for t, num in fromMap[f]:
                 used += num 
                 maxUsed = max(used, maxUsed)
+                if maxUsed > capacity:
+                    return False
                 heapq.heappush(ends, [t, num])
-
-        if maxUsed > capacity:
-            return False
         return True
