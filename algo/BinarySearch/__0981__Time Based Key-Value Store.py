38efc9d65744c9cf7495480aa6b610a703a496bf
Ian Lai <ian.explore.world@gmail.com>
2021-12-11 22:01:15 +0900

    Updated __0981__Time Based Key-Value Store.py

--- algo/BinarySearch/__0981__Time Based Key-Value Store.py
+++ algo/BinarySearch/__0981__Time Based Key-Value Store.py
@@ -1,9 +1,39 @@
+#2021/12/11
 class TimeMap:
-
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        print("Code-2")
+        self.tmap = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.tmap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.tmap:
+            return ""
+        arr = self.tmap[key]
+        
+        start, end = 0, len(arr) 
+        while start < end:
+            mid = start + (end - start) // 2
+            if arr[mid][0] < timestamp:
+                start = mid + 1
+            elif arr[mid][0] > timestamp:
+                end = mid
+            else:
+                return arr[mid][1]
+        return arr[start - 1][1] if start != 0 else ""
+        
+#========================================
+#2021/11/01
+class TimeMap1:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        print("Code-1")
         self.tmap = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
@@ -33,8 +63,6 @@ class TimeMap:
             return arr[r][1]
         else:
             return arr[l][1]
-            
-            
             
 
 # Your TimeMap object will be instantiated and called as such:
