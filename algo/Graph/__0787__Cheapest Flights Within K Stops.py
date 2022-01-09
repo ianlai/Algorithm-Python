f03e090a72e4ecb8d0ca08fa4a112fb2ef0bffc0
Ian Lai <ian.explore.world@gmail.com>
2022-01-09 15:39:40 +0900

    Updated __0787__Cheapest Flights Within K Stops.py

--- algo/Graph/__0787__Cheapest Flights Within K Stops.py
+++ algo/Graph/__0787__Cheapest Flights Within K Stops.py
@@ -14,17 +14,96 @@ class Solution:
     # 4
     
     # Test case:
-#     17
-# [[0,12,28],[5,6,39],[8,6,59],[13,15,7],[13,12,38],[10,12,35],[15,3,23],[7,11,26],[9,4,65],[10,2,38],[4,7,7],[14,15,31],[2,12,44],[8,10,34],[13,6,29],[5,14,89],[11,16,13],[7,3,46],[10,15,19],[12,4,58],[13,16,11],[16,4,76],[2,0,12],[15,0,22],[16,12,13],[7,1,29],[7,14,100],[16,1,14],[9,6,74],[11,1,73],[2,11,60],[10,11,85],[2,5,49],[3,4,17],[4,9,77],[16,3,47],[15,6,78],[14,1,90],[10,5,95],[1,11,30],[11,0,37],[10,4,86],[0,8,57],[6,14,68],[16,8,3],[13,0,65],[2,13,6],[5,13,5],[8,11,31],[6,10,20],[6,2,33],[9,1,3],[14,9,58],[12,3,19],[11,2,74],[12,14,48],[16,11,100],[3,12,38],[12,13,77],[10,9,99],[15,13,98],[15,12,71],[1,4,28],[7,0,83],[3,5,100],[8,9,14],[15,11,57],[3,6,65],[1,3,45],[14,7,74],[2,10,39],[4,8,73],[13,5,77],[10,0,43],[12,9,92],[8,2,26],[1,7,7],[9,12,10],[13,11,64],[8,13,80],[6,12,74],[9,7,35],[0,15,48],[3,7,87],[16,9,42],[5,16,64],[4,5,65],[15,14,70],[12,0,13],[16,14,52],[3,10,80],[14,11,85],[15,2,77],[4,11,19],[2,7,49],[10,7,78],[14,6,84],[13,7,50],[11,6,75],[5,10,46],[13,8,43],[9,10,49],[7,12,64],[0,10,76],[5,9,77],[8,3,28],[11,9,28],[12,16,87],[12,6,24],[9,15,94],[5,7,77],[4,10,18],[7,2,11],[9,5,41]]
-# 13
-# 4
-# 13
+    #     17
+    # [[0,12,28],[5,6,39],[8,6,59],[13,15,7],[13,12,38],[10,12,35],[15,3,23],[7,11,26],[9,4,65],[10,2,38],[4,7,7],[14,15,31],[2,12,44],[8,10,34],[13,6,29],[5,14,89],[11,16,13],[7,3,46],[10,15,19],[12,4,58],[13,16,11],[16,4,76],[2,0,12],[15,0,22],[16,12,13],[7,1,29],[7,14,100],[16,1,14],[9,6,74],[11,1,73],[2,11,60],[10,11,85],[2,5,49],[3,4,17],[4,9,77],[16,3,47],[15,6,78],[14,1,90],[10,5,95],[1,11,30],[11,0,37],[10,4,86],[0,8,57],[6,14,68],[16,8,3],[13,0,65],[2,13,6],[5,13,5],[8,11,31],[6,10,20],[6,2,33],[9,1,3],[14,9,58],[12,3,19],[11,2,74],[12,14,48],[16,11,100],[3,12,38],[12,13,77],[10,9,99],[15,13,98],[15,12,71],[1,4,28],[7,0,83],[3,5,100],[8,9,14],[15,11,57],[3,6,65],[1,3,45],[14,7,74],[2,10,39],[4,8,73],[13,5,77],[10,0,43],[12,9,92],[8,2,26],[1,7,7],[9,12,10],[13,11,64],[8,13,80],[6,12,74],[9,7,35],[0,15,48],[3,7,87],[16,9,42],[5,16,64],[4,5,65],[15,14,70],[12,0,13],[16,14,52],[3,10,80],[14,11,85],[15,2,77],[4,11,19],[2,7,49],[10,7,78],[14,6,84],[13,7,50],[11,6,75],[5,10,46],[13,8,43],[9,10,49],[7,12,64],[0,10,76],[5,9,77],[8,3,28],[11,9,28],[12,16,87],[12,6,24],[9,15,94],[5,7,77],[4,10,18],[7,2,11],[9,5,41]]
+    # 13
+    # 4
+    # 13
     
-    #====================================
+    # 2022/01/09 
+    # Dijkstra's variant (DP, with pruning) [5%]
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        print("Code5: Dijkstra (DP, with pruning)")
+        
+        # Generate graph 
+        graph = collections.defaultdict(dict)
+        for u, v, w in flights:
+            graph[u][v] = w
+        
+        #costMap = {}    # (node, stop) -> distance
+        maxStopMap = {}  # node -> maxStop
+        
+        heap = [(0, k + 1, src)]
+        res = inf
+        while heap:
+            cost, stop, node = heapq.heappop(heap)
+            
+            if stop < 0:
+                continue
+                
+            ### Pruning on maxStopMap (including the L48 memoization part, thus costMap is not needed anymore)
+            if node in maxStopMap and stop <= maxStopMap[node]:
+                continue
+                
+            ### Memoization 
+            #if (node, stop) in costMap:
+            #    continue
+            
+            ### Relaxation on maxStopMap
+            if node in maxStopMap:
+                maxStopMap[node] = max(maxStopMap[node], stop)
+            else:
+                maxStopMap[node] = stop
+                
+            ### Relaxation on costMap    
+            #costMap[(node, stop)] = cost
+            
+            if node == dst:
+                res = min(res, cost)
+                
+            for nxt in graph[node].keys():
+                heapq.heappush(heap, (cost + graph[node][nxt], stop - 1, nxt))
+        return res if res != inf else -1
+        
+    #========================================================================
+    
+    # 2022/01/09 
+    # Dijkstra's variant (DP, no pruning) [5%]
+    def findCheapestPrice4(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        print("Code4: Dijkstra (DP, no pruning)")
+        
+        # Generate graph 
+        graph = collections.defaultdict(dict)
+        for u, v, w in flights:
+            graph[u][v] = w
+        
+        costMap = {}   #(node, stop) -> distance
+        heap = [(0, k + 1, src)]
+        res = inf
+        while heap:
+            cost, stop, node = heapq.heappop(heap)
+            
+            if stop < 0:
+                continue
+            if (node, stop) in costMap:
+                #if cost < costMap[(node, stop)]:
+                #    costMap[(node, stop)] = cost
+                continue
+            else:
+                costMap[(node, stop)] = cost
+            if node == dst:
+                res = min(res, cost)
+                
+            for nxt in graph[node].keys():
+                heapq.heappush(heap, (cost + graph[node][nxt], stop - 1, nxt))
+        #print(costMap)
+        return res if res != inf else -1
+        
+    #========================================================================
     
     # 2021/12/31
-    # Dijkstra's variant []
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    # Dijkstra's variant [45%]
+    def findCheapestPrice3(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         print("Code3: Dijkstra")
         if not flights:
             return 0
@@ -51,6 +130,7 @@ class Solution:
             #print("out: [node, time, stop]" , curNode, curTime, curStop)
             if curStop > k:
                 continue
+                
             if curTime < distanceList[curNode]:  
                 distanceList[curNode] = curTime  #update distanceList when pulling out
                 stopList[curNode] = curStop      #update stopList when pulling 
@@ -68,7 +148,7 @@ class Solution:
             
         return distanceList[dst] if distanceList[dst] != inf else -1
     
-    #====================================
+    #========================================================================
     
     # Top-Down DP [O(V^2*K) 20%]
     def findCheapestPrice2(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
