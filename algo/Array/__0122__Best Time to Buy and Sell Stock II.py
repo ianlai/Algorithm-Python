7bc760f9ee769938a4a26c1a222038d13e6512ab
Ian Lai <ian.explore.world@gmail.com>
2022-01-13 22:28:34 +0900

    Updated __0122__Best Time to Buy and Sell Stock II.py, __0151__Reverse Words in a String.py

--- algo/Array/__0122__Best Time to Buy and Sell Stock II.py
+++ algo/Array/__0122__Best Time to Buy and Sell Stock II.py
@@ -1,5 +1,36 @@
 class Solution:
+    
+    # 2022/01/13
+    # Single pass, sum up all diff between i and i-1  (Approach3) [O(n): 58%]
     def maxProfit(self, prices: List[int]) -> int:
+        print("Code3")
+        profit = 0
+        for i in range(1, len(prices)):
+            diff = prices[i] -  prices[i-1] 
+            profit += prices[i] - prices[i-1] if diff > 0 else 0
+        return profit
+        
+    # ========================================
+    # 2022/01/13
+    # Single pass, sum up the diff between valley and peak (Approach2) [O(n): 24%]
+    def maxProfit2(self, prices: List[int]) -> int:
+        print("Code2")
+        profit = 0 
+        for i, v in enumerate(prices):
+            if i == 0:
+                buy = (i, v)
+            if i > 0 and prices[i-1] > prices[i]:
+                if i - 1 > buy[0] and prices[i-1] > buy[1]:
+                    profit += prices[i-1] - buy[1]
+                buy = (i, v)
+        profit += prices[-1] - buy[1]
+        return profit
+
+    # ========================================
+    # 2021/05/05
+    # Single pass [O(n):23%]
+    def maxProfit1(self, prices: List[int]) -> int:
+        print("Code1")
         if not prices or len(prices) == 1:
             return 0
 
--- algo/String/__0151__Reverse Words in a String.py
+++ algo/String/__0151__Reverse Words in a String.py
@@ -1,5 +1,29 @@
 class Solution:
+    
+    # 2022/01/13 
+    # Using reverse(), join() [O(n): 99%]
     def reverseWords(self, s: str) -> str:
+        if not s:
+            return s
+        
+        arr = []
+        cur = ""
+        for i, ch in enumerate(s):
+            if ch == " ":
+                if cur:
+                    arr.append(cur)
+                    cur = ""
+            else:
+                cur += ch
+        if cur:
+            arr.append(cur)
+        return " ".join(arr[::-1])
+                
+    # ==========================================
+    # 2021/06/09 
+    # Using split(), reverse(), and join() [O(n): 18%]
+    def reverseWords1(self, s: str) -> str:
+        print("Code1")
         if not s:
             return s
         
