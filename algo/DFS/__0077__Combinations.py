7bdd4bf17b0a5af2e9dea6555a3d3b77725beccd
Ian Lai <ian.explore.world@gmail.com>
2022-01-25 00:12:15 +0900

    Reviewed backtracking

--- algo/DFS/__0077__Combinations.py
+++ algo/DFS/__0077__Combinations.py
@@ -1,6 +1,41 @@
-class Solution:
+class Solution: 
     
+    # 2022/01/24
+    # Backtracking, using new cur every round [O(k * C(n, k)): 12%]
     def combine(self, n: int, k: int) -> List[List[int]]:
+        print("Code3")
+        def backtracking(n, k, cur, idx, res):
+            if len(cur) == k:
+                res.append(list(cur))
+            for i in range(idx, n):
+                backtracking(n, k, cur + [i+1], i+1, res)
+        res = []
+        backtracking(n, k, [], 0, res)
+        return res
+        
+    # =======================================
+
+    # 2022/01/24
+    # Backtracking, using same cur until last [O(k * C(n, k)): 8%]
+    def combine2(self, n: int, k: int) -> List[List[int]]:
+        print("Code2")
+        def backtracking(n, k, cur, idx, res):
+            if len(cur) == k:
+                res.append(list(cur))
+            for i in range(idx, n):
+                cur.append(i+1) #add (position:i, number: i+i)
+                backtracking(n, k, cur, i+1, res)
+                cur.remove(i+1)  
+            
+        res = []
+        backtracking(n, k, [], 0, res)
+        return res
+        
+    # =======================================
+    
+    # 2021/04/30 
+    def combine1(self, n: int, k: int) -> List[List[int]]:
+        print("Code1")
         if n <= 0 or k <= 0:
             return []
         
@@ -15,23 +50,4 @@ class Solution:
         
         for i in range(idx+1, n+1):
             self.dfs(n, k, i, cur + [i], res)
-
-    # ========================================  
-
-    # Backtracking [30%]
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        if n <= 0 or k <= 0:
-            return []
-        
-        ans = []
-        self.backtracking(n, k, 0, [], ans)
-        return ans
-    
-    def backtracking(self, n, k, idx, cur, ans):
-        if len(cur) == k:
-            ans.append(cur)
-            return 
-        
-        for i in range(idx, n):
-            self.backtracking(n, k, i + 1, cur + [i + 1], ans)
         
--- algo/DFS/__0078__Subsets.py
+++ algo/DFS/__0078__Subsets.py
@@ -1,7 +1,44 @@
 class Solution:
     
-    # Backtracking [48%]
+    # 2022/01/24 
+    # Backtracking, split to n element in each step [O(n * 2^n): 17%]
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        print("Code3")
+        def backtracking(nums, cur, idx, res):
+            if idx == len(nums):
+                return 
+            
+            for i in range(idx, len(nums)):
+                res.append(cur + [nums[i]])
+                backtracking(nums, cur + [nums[i]], i + 1, res)
+        
+        res = [[]]
+        backtracking(nums, [], 0, res)
+        return res
+        
+    #=========================================================
+
+    # 2021/05/27
+    def subsets2(self, nums: List[int]) -> List[List[int]]:
+        print("Code2")
+        if not nums:
+            return []
+        
+        results = []
+        self.dfs(nums, -1, [], results)
+        return results
+    
+    def dfs(self, nums, idx, cur, results):
+        results.append(cur)
+        
+        for i in range(idx + 1, len(nums)):
+            self.dfs(nums, i, cur + [nums[i]], results)
+            
+    #=========================================================
+
+    # 2021/04/30
+    def subsets1(self, nums: List[int]) -> List[List[int]]:
+        print("Code1")
         if not nums:
             return []
         ans = []
@@ -15,4 +52,3 @@ class Solution:
         
         for i in range(idx, len(nums)):
             self.backtracking(nums, i + 1, cur + [nums[i]], ans)
-            
