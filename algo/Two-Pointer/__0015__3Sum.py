9deab3a2b062d48e1523d59395d568dc6fe91b01
Ian Lai <ian.explore.world@gmail.com>
2022-01-26 17:56:34 +0900

    Added __0015__3Sum.py

--- algo/Two-Pointer/__0015__3Sum.py
+++ algo/Two-Pointer/__0015__3Sum.py
@@ -2,7 +2,58 @@
 
 class Solution:
     
+    # 2022/01/26 
+    # Two sum function + Redanduncy removal [O(n): 44%]
+    # 放入時就去重
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        print("Code-3: Two sum function + Redanduncy removal")
+        if not nums or len(nums) <= 2:
+            return []
+    
+        def twoSum(nums, start, end, target):
+            if start >= end:
+                return []
+            left, right = start, end 
+            res = []
+            while left < right:
+                
+                # Redanduncy removal 
+                if left > start and nums[left-1] == nums[left]:
+                    left += 1
+                    continue
+                if right < end and nums[right] == nums[right+1]:
+                    right -= 1
+                    continue
+                    
+                twoSum = nums[left] + nums[right]
+                
+                if twoSum == target:
+                    res.append([nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                elif twoSum > target:
+                    right -= 1
+                else:
+                    left += 1
+            return res 
+        
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            # Redanduncy removal 
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+            twoSumList = twoSum(nums, i + 1, len(nums) - 1, -nums[i])
+            for twoSumPair in twoSumList:
+                twoSumPair.append(nums[i])
+                res.append(twoSumPair)
+        return res
+            
+    # =======================================
+
+    # 2021/10/13
     # Two-Sum, manaully remove the redundancies of each item BEFORE adding to result [O(n2): 80%]
+    # 結束後才去重
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         print("Code-2")
         nums.sort()
@@ -30,7 +81,7 @@ class Solution:
     # =======================================
 
     # Two-Sum, use set to remove the redundancies AFTER adding to result [O(n2): 19%]
-    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         print("Code-1")
         nums.sort()
         res = set()
