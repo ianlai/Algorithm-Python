10e4302450e69f43afa8e1d483b2e97f9b567590
Ian Lai <ian.explore.world@gmail.com>
2022-01-29 02:17:43 +0900

    Updated __0049__Group Anagrams.py

--- algo/HashMap/__0049__Group Anagrams.py
+++ algo/HashMap/__0049__Group Anagrams.py
@@ -1,7 +1,19 @@
 class Solution:
     
-    # Use sorted string as the map key
+    # 2022/01/28
+    # Use sorted string as the map key [94%]
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        print("Code2")
+        stringMap = collections.defaultdict(list)
+        for s in strs:
+            stringMap[''.join(sorted(s))].append(s)
+        return stringMap.values()
+
+    # ==================================================
+    # 2021/04/18
+    # Use sorted string as the map key [35%]
+    def groupAnagrams1(self, strs: List[str]) -> List[List[str]]:
+        print("Code1")
         if strs is None or len(strs) == 0:
             return []
         
@@ -19,4 +31,4 @@ class Solution:
             ans.append(keyToList[key])
 
         return ans
-            
+                    
