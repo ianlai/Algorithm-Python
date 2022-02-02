1a3b35778200d75287d7fdbf33af7153157f384e
Ian Lai <ian.explore.world@gmail.com>
2022-02-02 23:03:26 +0900

    Updated __0121__Best Time to Buy and Sell Stock.py

--- algo/DP/__0121__Best Time to Buy and Sell Stock.py
+++ algo/DP/__0121__Best Time to Buy and Sell Stock.py
@@ -1,7 +1,20 @@
 class Solution:
-    
-    # Find min from each point, compare prev and lastMin [O(n), 21%]
+
+    # 2022/02/02
+    # Buy at i-1, Profit = Sell - Buy at i [O(n): 36%]
     def maxProfit(self, prices: List[int]) -> int:
+        print("Code3")
+        buy, profit = inf, 0
+        for i in range(1, len(prices)):
+            buy = min(prices[i-1], buy)
+            profit = max(profit, prices[i] - buy)
+        return profit
+    
+    
+    # 2021/05/05 
+    # Find min from each point, compare prev and lastMin [O(n), 21%]
+    def maxProfit2(self, prices: List[int]) -> int:
+        print("Code2")
         if not prices or len(prices) == 1:
             return 0
     
