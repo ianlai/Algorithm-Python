d46ebcfb5f4bbec77a9a50d2ea99ec06962f9f60
Ian Lai <ian.explore.world@gmail.com>
2022-02-08 02:33:48 +0900

    Updated __0875__Koko Eating Bananas.py

--- algo/BinarySearch/__0875__Koko Eating Bananas.py
+++ algo/BinarySearch/__0875__Koko Eating Bananas.py
@@ -1,7 +1,30 @@
 class Solution:
-    
-    # Binary Search [O(logn): 53%]
+    # 2022/02/07
+    # Binary Search [O(logn): 23%]
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        print("Code2")
+        def canFinish(speed, h):
+            time = 0
+            for pile in piles:
+                time += math.ceil(pile / speed)
+            if time > h:
+                return False
+            return True
+            
+        start, end = 1, max(piles) + 1
+        while start < end:
+            mid = start + (end - start) // 2
+            if canFinish(mid, h):
+                end = mid 
+            else:
+                start = mid + 1
+        return start 
+        
+    # ========================================   
+    # 2021/12/11 
+    # Binary Search [O(logn): 53%]
+    def minEatingSpeed1(self, piles: List[int], h: int) -> int:
+        print("Code1")
         maxBanana = max(piles)
         start, end = 1, maxBanana
         while start < end:
