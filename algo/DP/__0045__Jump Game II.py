db5bc6a4f99408a9dd2b909e90a11f2fd0404ae5
Ian Lai <ian.explore.world@gmail.com>
2022-02-21 11:14:26 +0900

    Updated __0045__Jump Game II.py with Greedy

--- algo/DP/__0045__Jump Game II.py
+++ algo/DP/__0045__Jump Game II.py
@@ -1,8 +1,34 @@
 class Solution:
     
+    #2022/02/20
+    #Greedy[O(n): 79%]
+    def jump(self, nums: List[int]) -> int:
+        print("Code2")
+        if len(nums) <= 1:
+            return 0
+        dp = [inf] * len(nums)
+        dp[0] = 0
+        
+        curIdx = 0
+        nextStep = 0
+        while curIdx < len(nums):
+            furthestStep = 0
+            for step in range(1, nums[curIdx]+1):
+                nextIdx = curIdx + step
+                if nextIdx >= len(nums) - 1:
+                    return dp[curIdx] + 1
+                if nextIdx + nums[nextIdx] > furthestStep:
+                    furthestStep = nextIdx + nums[nextIdx] 
+                    nextStep = nextIdx
+            dp[nextStep] = dp[curIdx] + 1
+            curIdx = nextStep
+        return dp[-1]
+            
+    
     #2022/02/14
     #DP[O(n2): 5%]
-    def jump(self, nums: List[int]) -> int:
+    def jump1(self, nums: List[int]) -> int:
+        print("Code1")
         dp = [inf] * len(nums)
         dp[0] = 0
         for i, v in enumerate(nums):
