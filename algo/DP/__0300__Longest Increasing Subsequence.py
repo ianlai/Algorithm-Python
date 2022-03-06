d66e10630c69d2de15631d44570f5f02ef285311
Ian Lai <ian.explore.world@gmail.com>
2022-03-06 18:06:27 +0900

    Updated __0300__Longest Increasing Subsequence.py with BS

--- algo/DP/__0300__Longest Increasing Subsequence.py
+++ algo/DP/__0300__Longest Increasing Subsequence.py
@@ -1,7 +1,29 @@
 class Solution:
     
-    # DP [O(n2): 9%]
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    # 2022/03/06
+    # Build smallest-val subsequence with Binary Search  [O(nlogn): 98%]
+    # Patience Sorting 
+    # https://leetcode.com/problems/longest-increasing-subsequence/discuss/74848/9-lines-C++-code-with-O(NlogN)-complexity/337602
+    # https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+    def lengthOfLIS(self, nums: List[int]) -> int:    
+        print("Code2")
+        if not nums:
+            return 0
+        subsequence = [nums[0]]
+        for i in range(1, len(nums)):
+            v = nums[i]
+            if v > subsequence[-1]:
+                subsequence.append(v)
+            else:
+                idx = bisect.bisect_left(subsequence, v)
+                subsequence[idx] = v
+        return len(subsequence)
+                       
+    # ==========================================================
+    # 2021/07/08 
+    # DP [O(n2): 46%]
+    def lengthOfLIS1(self, nums: List[int]) -> int:
+        print("Code1")
         if not nums:
             return 0
         
