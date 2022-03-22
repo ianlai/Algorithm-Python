0c05bd8de1cd7c498901157db7f20606c55288ba
Ian Lai <ian.explore.world@gmail.com>
2022-03-22 18:03:24 +0900

    Updated __0973__K Closest Points to Origin.py with QuickSelect

--- algo/Heap/__0973__K Closest Points to Origin.py
+++ algo/Heap/__0973__K Closest Points to Origin.py
@@ -1,9 +1,39 @@
 import heapq
 class Solution:
     
-    # max-heap [O(k + (n-k)logk), 77%]
+    # 2022/03/22
+    # Quick select [O(nlogn) worst:O(n2) 52%] 
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        print("max-heap")
+        print("Code3: Quickselect")
+        distances = [(p1**2 + p2**2, (p1, p2)) for p1, p2 in points]
+        def findTopK(arr, start, end, k):
+            l, r = start, end
+            mid = start + (end - start) // 2
+            pivot = arr[mid][0]
+            while l <= r:
+                while l <= r and arr[l][0] < pivot:
+                    l += 1
+                while l <= r and arr[r][0] > pivot:
+                    r -= 1
+                if l <= r:
+                    arr[l], arr[r] = arr[r], arr[l]
+                    l += 1
+                    r -= 1
+            if k <= r:
+                return findTopK(arr, start, r, k)
+            elif k >= l:
+                return findTopK(arr, l, end, k)
+            else:
+                return arr[:k+1]
+        closests = findTopK(distances, 0, len(distances)-1, k-1)
+        res = [p[1] for p in closests]
+        return res
+                
+    # ================================
+    # 2021/06/05
+    # max-heap [O(k + (n-k)logk), 77%]
+    def kClosest2(self, points: List[List[int]], k: int) -> List[List[int]]:
+        print("Code2: max-heap")
         if not points or k == 0:
             return []
         
@@ -26,8 +56,8 @@ class Solution:
     # ================================
     
     # min-heap [O(n + klogn), 76%]
-    def kClosest2(self, points: List[List[int]], k: int) -> List[List[int]]:
-        print("min-heap")
+    def kClosest1(self, points: List[List[int]], k: int) -> List[List[int]]:
+        print("Code1: min-heap")
         if not points or k == 0:
             return []
         
