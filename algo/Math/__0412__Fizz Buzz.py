fc4e7783879fbfe622848db331e44b6190aa3c00
Ian Lai <ian.explore.world@gmail.com>
2022-03-27 20:35:18 +0900

    Updated __0412__Fizz Buzz.py

--- algo/Math/__0412__Fizz Buzz.py
+++ algo/Math/__0412__Fizz Buzz.py
@@ -1,5 +1,30 @@
 class Solution:
+    
     # 2022/03/27
+    # Avoid using mod operand [O(n): 50%]
+    def fizzBuzz(self, n: int) -> List[str]:
+        print("Code3")
+        res = []
+        count3, count5 = 3, 5
+        for i in range(1, n+1):
+            addString = ""
+            count3 -= 1
+            count5 -= 1
+            if count3 == 0:
+                addString += "Fizz"
+                count3 = 3
+            if count5 == 0:
+                addString += "Buzz"
+                count5 = 5
+            if addString:
+                res.append(addString)
+            else:
+                res.append(str(i))
+        return res
+    # =====================================
+    
+    # 2022/03/27
+    # Form the added string first [O(n): 36%]
     def fizzBuzz(self, n: int) -> List[str]:
         print("Code2")
         res = []
@@ -14,10 +39,11 @@ class Solution:
             else:
                 res.append(str(i))
         return res
-    
     # =====================================
+    
     # 2022/03/26
-    def fizzBuzz1(self, n: int) -> List[str]:
+    # Three separated cases [O(n): 22%]
+    def fizzBuzz(self, n: int) -> List[str]:
         print("Code1")
         res = []
         for i in range(1, n+1):
