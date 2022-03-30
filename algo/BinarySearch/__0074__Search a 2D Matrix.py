7e4e6dfc7a008513dd13ba81e320f19ffa41cb0c
Ian Lai <ian.explore.world@gmail.com>
2022-03-30 23:55:23 +0900

    Updated __0074__Search a 2D Matrix.py

--- algo/BinarySearch/__0074__Search a 2D Matrix.py
+++ algo/BinarySearch/__0074__Search a 2D Matrix.py
@@ -1,7 +1,30 @@
 class Solution:
-    
-    # Binary Search; Transform matrix to list [O(n), 57%]
+    # 2022/03/30 
+    # Binary Search with matrix [Time: O(logmn): 87% / Space: O(1): 8%]
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        print("Code2")
+        def convert(idx):
+            i, j = idx // n, idx % n
+            return i, j
+        
+        m, n = len(matrix), len(matrix[0])
+        start, end = 0, m * n 
+        while start < end:
+            mid = start + (end - start) // 2
+            midi, midj = convert(mid)
+            if matrix[midi][midj] == target:
+                return True
+            elif matrix[midi][midj] < target:
+                start = mid + 1
+            else:
+                end = mid 
+        return False
+         
+        
+    # 2021/05/11
+    # Convert to array then binary Search [Time: O(logmn): 59% / Space: O(mn): 8%]
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        print("Code1")
         if not matrix or not matrix[0]:
             return False
         
