16573b3b4c253895602372b815264913ec0f1bd7
Ian Lai <ian.explore.world@gmail.com>
2022-04-13 23:01:33 +0900

    Added __0106__Construct Binary Tree from Inorder and Postorder Traversal.py

--- algo/Tree/__0106__Construct Binary Tree from Inorder and Postorder Traversal.py
+++ algo/Tree/__0106__Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -5,10 +5,52 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
+    # ===============================================
+    # 2022/04/13
+    # Not passing slice + Not using index() [O(n): 90%]
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        print("Code3")
+        
+        def helper(left, right):
+            if left > right:
+                return None
+            node = TreeNode(postorder.pop())
+            #idx = inorder.index(node.val)
+            idx = valToIdx[node.val]
+            
+            node.right = helper(idx+1, right)
+            node.left = helper(left, idx-1)
+            return node 
+        
+        valToIdx = {}
+        for i, v in enumerate(inorder):
+            valToIdx[v] = i
+            
+        return helper(0, len(inorder)-1)
+        
+    # ===============================================
+    # 2022/04/13
+    # Not passing slice [O(n): 63%]
+    def buildTree2(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        print("Code2")
+        
+        def helper(left, right):
+            if left > right:
+                return None
+            node = TreeNode(postorder.pop())
+            idx = inorder.index(node.val)
+            
+            node.right = helper(idx+1, right)
+            node.left = helper(left, idx-1)
+            return node 
+        
+        return helper(0, len(inorder)-1)
+        
+    # ===============================================
     # 2022/04/12
     # Using slice, improvable  [O(n2): 20%]
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+    def buildTree1(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        print("Code1")
         if not inorder:
             return None 
         
