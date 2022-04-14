79a6213ce1e78c30bb5728a4b576f715ae096795
Ian Lai <ian.explore.world@gmail.com>
2022-04-14 21:46:46 +0900

    Updated __0700__Search in a Binary Search Tree.py

--- algo/Tree/__0700__Search in a Binary Search Tree.py
+++ algo/Tree/__0700__Search in a Binary Search Tree.py
@@ -5,10 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
+    # 2022/04/14
+    # Iteration [O(logn): 80%]
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        print("Code2")
+        
+        while root and root.val != val:
+            if val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return root 
+        
+        
     # 2022/03/25
     # Recursion [O(logn): 25%]
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    def searchBST1(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        print("Code1")
         if root is None:
             return None
         if val == root.val:
