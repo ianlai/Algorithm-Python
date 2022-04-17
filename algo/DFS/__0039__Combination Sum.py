1f7eecf1d3f307239ecf17b064c3e53e17e67f9d
Ian Lai <ian.explore.world@gmail.com>
2022-04-17 17:58:12 +0900

    Updated __0039__Combination Sum.py

--- algo/DFS/__0039__Combination Sum.py
+++ algo/DFS/__0039__Combination Sum.py
@@ -1,25 +1,29 @@
 class Solution:
 
-    # Backtracking [O(n^(target/min)): 63%]
+    # 2022/04/17
+    # Backtracking [Time: O( N^(target/min +1) ) : 63% / Space: O(target/min): 60%] 
+    # 樹高h = O(target/min) <- space
+    # 節點數 = O( N^(target/min +1) ) <- time 
+    # 時間 = 節點樹
+    # 空間 = 樹深 + cur = O(target/min) + O(target/min) = O(target/min)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        print("Code2")
         if not candidates or target <= 0:
             return []
         
+        def backtracking(candidates, target, idx, cur, res):
+            if target == 0:
+                res.append(cur)
+                return
+            if target < 0:
+                return 
+            for i in range(idx, len(candidates)):
+                backtracking(candidates, target - candidates[i], i, cur + [candidates[i]], res)
+
         res = []
-        self.backtracking(candidates, target, 0, [], res)
+        backtracking(candidates, target, 0, [], res)
         return res
-    
-    def backtracking(self, candidates, target, idx, cur, res):
-        if target == 0:
-            res.append(cur)
-            return
-        if target < 0:
-            return 
-            
-        for i in range(idx, len(candidates)):
-            chosen = candidates[i]
-            self.backtracking(candidates, target - chosen, i, cur + [chosen], res)
-    
+
     # ==============================================================
     
     # Backtracking [48%]
