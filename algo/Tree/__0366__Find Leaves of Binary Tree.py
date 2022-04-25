d9b3b4d8dd97be51e3adf696745781c15a1247f1
Ian Lai <ian.explore.world@gmail.com>
2022-04-25 18:58:20 +0900

    Updated __0366__Find Leaves of Binary Tree.py

--- algo/Tree/__0366__Find Leaves of Binary Tree.py
+++ algo/Tree/__0366__Find Leaves of Binary Tree.py
@@ -6,8 +6,39 @@
 #         self.right = right
 class Solution:
     
-    # DFS [O(n), 97%]
+    # 2022/04/25
+    # Postorder DFS [Time: O(n + hlogh): 53% / Space: O(n + h): 27%]
     def findLeaves(self, root: TreeNode) -> List[List[int]]:
+        print("Code2")
+        
+        def dfs(node, levelToNodes):
+            if node is None:
+                return 0
+            
+            left  = dfs(node.left, levelToNodes)
+            right = dfs(node.right, levelToNodes)
+            
+            level = max(left, right) + 1
+            levelToNodes[level].append(node.val)
+            return level
+        
+        if not root:
+            return []
+        
+        levelToNodes = collections.defaultdict(list)
+        dfs(root, levelToNodes)
+        
+        res = []
+        for key in sorted(levelToNodes.keys()):
+            res.append(levelToNodes[key])
+        return res
+    
+    # ==========================================================
+    
+    # 2021/07/01 
+    # Postorder DFS (no sorting) [Time: O(n): 77% / Space: O(n + h): 76%]
+    def findLeaves(self, root: TreeNode) -> List[List[int]]:
+        print("Code1")
         if not root:
             return []
         res = []
