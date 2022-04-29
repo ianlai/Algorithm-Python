a0dd6b6c005b34d11580657a3cee119054e35f33
Ian Lai <ian.explore.world@gmail.com>
2022-04-29 21:17:58 +0900

    Updated __0017__Letter Combinations of a Phone Number.py

--- algo/DFS/__0017__Letter Combinations of a Phone Number.py
+++ algo/DFS/__0017__Letter Combinations of a Phone Number.py
@@ -1,7 +1,43 @@
 class Solution:
     
-    # Backtracking [O(4^N), 94%]
+    # 2022/04/28 
+    # DFS, backtracking [O(): 37% / O(): 32%]
     def letterCombinations(self, digits: str) -> List[str]:
+        print("Code2")
+
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        
+        def dfs(idx, cur, res):
+            if idx == len(digits):
+                res.append("".join(cur))
+                return res
+            
+            for ch in digitToChar[digits[idx]]:
+                cur.append(ch)
+                dfs(idx+1, cur, res)
+                cur.pop()
+                
+        if not digits:
+            return []
+        
+        res = []
+        dfs(0, [], res)
+        return res
+    
+    # ==========================================
+    # 2021/05/03 
+    # Backtracking [O(4^N), 94%]
+    def letterCombinations1(self, digits: str) -> List[str]:
+        print("Code1")
         if len(digits) == 0:
             return []
         dToM = {
@@ -26,4 +62,4 @@ class Solution:
         numberOfChar = len(dToM[digits[idx]])
         for i in range(numberOfChar):
             char = dToM[digits[idx]][i]
-            self.backtracking(digits, dToM, idx + 1, cur + [char], ans)    
+            self.backtracking(digits, dToM, idx + 1, cur + [char], ans)  
