46721d8764db3fa02c08d7ed17b0d6a26b11cf2d
Ian Lai <ian.explore.world@gmail.com>
2022-05-08 20:02:22 +0900

    Updated __0127__Word Ladder.py

--- algo/BFS/__0127__Word Ladder.py
+++ algo/BFS/__0127__Word Ladder.py
@@ -1,9 +1,107 @@
 class Solution:
     
+    '''
+    解答的作法，速度最快。這題就是做BFS，但考點關鍵在於怎麼找到相連的字。
+    一個目標字要找到相連的其他字所需時間：
+    (1) 從wordList內掃描: O(WL) 
+    (2) 對目標字的每一個字元做26字母變換: O(26L)
+    (3) 把目標字轉成代表字，再把每個代表字存的array取出: O(LL) //但需要preprocessing把每個字存入相對應代表字array
+    '''
+    # BFS + list alphabet combination [O(WLL + WLL): 73% / O(WLL): 58%]
+    def ladderLength(self, beginWord, endWord, wordList):
+        print("Code7 (fastest)")
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+        # Since all words are of same length.
+        L = len(beginWord)
+        
+        # {"h*t": ["hot", "hat", "hit"]}
+        # Form map -> TC: O(WLL)
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word) #TC: O(L)
+
+        # BFS -> TC: O(WLL)
+        queue = collections.deque([(beginWord, 1)])
+        visited = set([beginWord])
+        while queue:
+            current_word, level = queue.popleft()
+            for i in range(L):
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+                for word in all_combo_dict[intermediate_word]:
+                    if word == endWord:
+                        return level + 1
+                    if word not in visited:
+                        visited.add(word)
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []
+        return 0
+    
+    
+    # 2022/05/08
+    # BFS + list alphabet combination [O(W*L*26): 15% / O(W): 58%]
+    # 用visited判斷是否走過，距離則直接放在node放入deq
+    def ladderLength6(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        print("Code6 (ok)")
+        def findNextWords(word, wordSet):
+            nextWords = []
+            for i in range(len(word)):
+                for j in range(26):
+                    ch = chr(ord('a') + j)
+                    cur = word[:i] + ch + word[i+1:]
+                    if cur in wordSet:
+                        nextWords.append(cur)
+            return nextWords
+            
+        wordSet = set(wordList)
+        visited = set(beginWord)
+        deq = collections.deque([(beginWord, 1)])
+        while deq:
+            cur, level = deq.popleft()
+            for nxt in findNextWords(cur, wordSet):
+                if nxt in visited:
+                    continue
+                visited.add(nxt)
+                if nxt == endWord:
+                    return level + 1
+                deq.append((nxt, level + 1))
+        return 0
+    
+    # 2022/05/08
+    # BFS + list alphabet combination [O(W*L*26): 15% / O(W): 58%]
+    # 用distance map來做到存visited和level的效果
+    def ladderLength5(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        print("Code5")
+        def findNextWords(word, wordSet):
+            nextWords = []
+            for i in range(len(word)):
+                for j in range(26):
+                    ch = chr(ord('a') + j)
+                    cur = word[:i] + ch + word[i+1:]
+                    if cur in wordSet:
+                        nextWords.append(cur)
+            return nextWords
+            
+        wordSet = set(wordList)
+        distance = {beginWord: 1}
+        deq = collections.deque([beginWord])
+        while deq:
+            cur = deq.popleft()
+            for nxt in findNextWords(cur, wordSet):
+                if nxt in distance:
+                    continue
+                distance[nxt] = distance[cur] + 1
+                if nxt == endWord:
+                    return distance[nxt]
+                deq.append(nxt)
+        return 0
+                
+        
     # 2021/10/19 
     # BFS + Search whole wordList [O(W*L*26): 41%]
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        print("BFS + list possible neighbor words")
+    def ladderLength4(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        print("Code4: BFS + list possible neighbor words")
         def findNextWords(cur):
             res = []
             for i in range(len(cur)):
