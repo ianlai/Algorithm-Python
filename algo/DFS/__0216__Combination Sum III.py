957f5e4cba34bd4b762eea93956d8b2d0f2ba263
Ian Lai <ian.explore.world@gmail.com>
2022-05-10 23:34:53 +0900

    Updated __0216__Combination Sum III.py

--- algo/DFS/__0216__Combination Sum III.py
+++ algo/DFS/__0216__Combination Sum III.py
@@ -1,5 +1,51 @@
 class Solution:
+    '''
+    TC: O(9^k) -> O( C(9,K) ) = O(9!/(9-k)!/k!)
+    SC: O(k)
+    '''
+    # 2022/05/10
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        print("Backtracking - func")
+        target = n
+        if not 0 < target <= 45 :
+            return []
+        
+        res = []
+        def dfs(digit, target, k, cur):
+            if target == 0 and k == 0:
+                nonlocal res
+                res.append(list(cur))
+                return 
+            if target < 0 or k == 0:
+                return 
+            for nxt in range(digit + 1, 10):
+                dfs(nxt, target - nxt, k - 1, cur + [nxt])
+                
+        dfs(0, target, k, [])
+        return res 
     
+    def combinationSum32(self, k: int, n: int) -> List[List[int]]:
+        print("Backtracking - for loop")
+        target = n
+        if target > 45 or target < 0:
+            return []
+        
+        res = []
+        def dfs(digit, target, k, cur):
+            for nxt in range(digit + 1, 10):
+                if target == nxt and k == 1:
+                    nonlocal res
+                    res.append(list(cur + [nxt])) 
+                    continue
+                if target < 0 or k == 0:
+                    continue  
+                dfs(nxt, target - nxt, k - 1, cur + [nxt])
+                
+        dfs(0, target, k, [])
+        return res 
+    
+    # ====================================  
+
     # DFS [O((9!)/k!(9-k)!), 96%]
     # DFS loop has (9 - layer) choices 
     def combinationSum31(self, k: int, n: int) -> List[List[int]]:
@@ -26,7 +72,7 @@ class Solution:
     
     # DFS [O((9!)/k!(9-k)!), 63%]
     # DFS loop has Yes/No choices 
-    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+    def combinationSum30(self, k: int, n: int) -> List[List[int]]:
         print("Choose take it or not in each layer (Yes/No)")
         if k == 0 or n == 0:
             return []
@@ -42,12 +88,14 @@ class Solution:
         if len(cur) > k:
             return 
         
-        #print(cur)
+        print(cur)
         if len(cur) == k:
             if n == 0:
                 results.append(cur)
                 return
-            
+        
         val = idx + 1
+        if idx == 9:
+            print("  idx=", idx, " val=", val)
         self.dfs2(k, n, idx + 1, cur, results)
         self.dfs2(k, n - val, idx + 1, cur + [val], results)
