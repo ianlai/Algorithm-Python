bbd7680009ca4e6f84ef93af780f94b6101c17b3
Ian Lai <ian.explore.world@gmail.com>
2022-05-11 23:33:20 +0900

    Updated __1641__Count Sorted Vowel Strings.py

--- algo/DP/__1641__Count Sorted Vowel Strings.py
+++ algo/DP/__1641__Count Sorted Vowel Strings.py
@@ -1,9 +1,73 @@
 class Solution:
+
+    # Math 
+    '''
+    H(5, n) = C(5+n-1, n) = C(n+4, n) = (n+4)(n+3)(n+2)(n+1) / 24 
+    '''
+    
+    # DFS 
+    ''' 
+    1 - 2  -- 3
+           -- 4
+           -- 5
+      - 3  -- 4
+           -- 5
+      - 4  -- 5
+      - 5
+      
+    2 - 3 -- 4
+          -- 5
+      - 4 -- 5
+      - 5 
+      
+    3 - 4
+      - 5
+      
+    4 - 5
+    
+    5 
+    
+    '''
+    
+    #DP 
+    '''
+        a  e  i  o  u
+    1:  1  1  1  1  1 
+    2:  1  2  3  4  5
+    3:  1  3  6 10 15
+    
+    '''  
+    
+    # 2022/05/11
+    # Math 重複組合 [O(1): 95% / O(1): 65%]
+    def countVowelStrings(self, n: int) -> int:
+        print("Code3 Math")
+        return (n+1)*(n+2)*(n+3)*(n+4) // 24
+
+
+    '''
+    2022/05/11 
+    DP (buttom-up)
+
+    TC: O(5N) = O(N)
+    SC: O(5)  = O(1)
+    '''
+    # 2022/05/11
+    # DP (buttom-up) [O(n): 23% / O(1): 27%]
+    def countVowelStrings(self, n: int) -> int:
+        print("Code2 [BEST]")
+        dp = [1] * 5
+        for i in range(n - 1):
+            for j in range(1, 5):
+                dp[j] += dp[j - 1] 
+        return sum(dp)
+    
     
     # 2022/03/08
     # DP [O(n): 68%]
     # 看起來不難，但需要兩個dp matrix(或省空間的array)，但還是想很久才找出關係式子
-    def countVowelStrings(self, n: int) -> int:
+    def countVowelStrings1(self, n: int) -> int:
+        print("Code1")
         if n == 1:
             return 5
         if n == 2:
