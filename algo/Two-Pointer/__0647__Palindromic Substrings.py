1dda284daf46f3e3de7720d2037b12427f4231aa
Ian Lai <ian.explore.world@gmail.com>
2022-05-22 22:26:33 +0900

    Updated __0647__Palindromic Substrings.py

--- algo/Two-Pointer/__0647__Palindromic Substrings.py
+++ algo/Two-Pointer/__0647__Palindromic Substrings.py
@@ -1,7 +1,32 @@
 class Solution:
     
-    # Two-Pointer 背向雙指針 [O(n2): 64%]
+    # 2022/05/22
+    # Two-Pointer [O(N2): 82% / O(1): 75%]
     def countSubstrings(self, s: str) -> int:
+        print("Code2")
+        def countPalindromes(left, right):
+            count = 0 
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    count += 1
+                else:
+                    break
+                left -= 1
+                right += 1
+            return count
+            
+        res = 0
+        for i in range(len(s)):
+            res += countPalindromes(i, i)
+            if i > 0:
+                res += countPalindromes(i - 1, i)
+        return res 
+        
+
+    # 2022/02/17
+    # Two-Pointer 背向雙指針 [O(n2): 64%]
+    def countSubstrings1(self, s: str) -> int:
+        print("Code1")
         def countPalindrome(start, end):
             count = 0
             while start >= 0 and end < len(s):
