8e31007a5c5c7735da18aa0d64df3cfb923a97b6
Ian Lai <ian.explore.world@gmail.com>
2022-05-26 23:29:20 +0900

    Updated __0300__Longest Increasing Subsequence.py

--- algo/DP/__0300__Longest Increasing Subsequence.py
+++ algo/DP/__0300__Longest Increasing Subsequence.py
@@ -1,22 +1,48 @@
 class Solution:
     
+    # 2022/05/26 
+    # Increasing Sub + Binary search [O(nlogn): 83% / O(n): 14%] 
+    def lengthOfLIS(self, nums: List[int]) -> int:    
+        print("Code3")
+        def binarySearchLeft(arr, v):
+            start, end = 0, len(arr)
+            while start < end:
+                mid = start + (end - start) // 2
+                if arr[mid] < v:
+                    start = mid + 1
+                else:
+                    end = mid 
+            return start
+        
+        seq = []
+        for v in nums:
+            idx = binarySearchLeft(seq, v)
+            if idx == len(seq):
+                seq.append(v)
+            else:
+                seq[idx] = v
+        return len(seq)
+            
+        
     # 2022/03/06
     # Build smallest-val subsequence with Binary Search  [O(nlogn): 98%]
     # Patience Sorting 
     # https://leetcode.com/problems/longest-increasing-subsequence/discuss/74848/9-lines-C++-code-with-O(NlogN)-complexity/337602
     # https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
-    def lengthOfLIS(self, nums: List[int]) -> int:    
+    def lengthOfLIS2(self, nums: List[int]) -> int:    
         print("Code2")
         if not nums:
             return 0
         subsequence = [nums[0]]
         for i in range(1, len(nums)):
+            #print(i, "  ", subsequence)
             v = nums[i]
             if v > subsequence[-1]:
                 subsequence.append(v)
             else:
                 idx = bisect.bisect_left(subsequence, v)
                 subsequence[idx] = v
+            #print(i, "__", subsequence)
         return len(subsequence)
                        
     # ==========================================================
