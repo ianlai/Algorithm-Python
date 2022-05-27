163853a2f7e0d3c8dff678e2a60caf2a30415af2
Ian Lai <ian.explore.world@gmail.com>
2022-05-28 02:37:52 +0900

    Updated __0023__Merge k Sorted Lists.py

--- algo/LinkedList/__0023__Merge k Sorted Lists.py
+++ algo/LinkedList/__0023__Merge k Sorted Lists.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from queue import PriorityQueue
 import heapq
 
 class Node:
@@ -11,15 +12,76 @@ class Node:
         self.node = node
         
     def __lt__(self, other):
-        if self.val <= other.val:
+        if self.val < other.val:
             return True
         return False
     
+
+
 class Solution:
     
-    #Priority Queue + Implement a wrapper [O(nlogk): 41%]
+    # 2022/05/28 
+    # PriorityQueue + override lt func [O(NlogL): 18% / O(L): 36%]
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        print("Priority Queue")
+        print("Code4")
+        ListNode.__lt__ = lambda self, y: self.val < y.val
+
+        pq = PriorityQueue()
+        for l in lists:
+            if l:
+                pq.put(l)
+        
+        dummy = ListNode()
+        cur = dummy 
+        while not pq.empty():
+            pop = pq.get()
+            cur.next = pop
+            cur = cur.next 
+            pop = pop.next
+            if pop:
+                pq.put(pop)
+        return dummy.next
+    
+    # ================================================
+
+    # 20221/08/10 
+    # Priority Queue + Implement a wrapper [O(nlogk): 41%]
+    def mergeKLists3(self, lists: List[ListNode]) -> ListNode:
+        print("Code3: Priority Queue (improvement)")
+        
+        res = ListNode(0)
+        p1 = res
+        hq = []
+        numOfEnded = 0
+        
+        for head in lists:
+            if not head:
+                numOfEnded +=1 
+                continue
+            
+            #heapq.heappush(hq, [head.val, head])   #array: val, node
+            n = Node(head.val, head)                #wrap in a node class
+            heapq.heappush(hq, n)   #val, node
+                
+        while numOfEnded < len(lists): 
+            n = heapq.heappop(hq)
+            node = n.node
+            p1.next = node
+            p1 = p1.next
+            if node.next:
+                node = node.next
+                n = Node(node.val, node)
+                heapq.heappush(hq, n)
+            else:
+                numOfEnded += 1
+        return res.next
+    
+    
+    # ================================================
+        
+    #Priority Queue + Implement a wrapper [O(nlogk): 41%]
+    def mergeKLists2(self, lists: List[ListNode]) -> ListNode:
+        print("Code2: Priority Queue")
         if not lists:
             return None
         
@@ -54,7 +116,7 @@ class Solution:
             
     # Merge lists one by one [O(kN): 7%]
     def mergeKLists1(self, lists: List[ListNode]) -> ListNode:
-        print("Merge lists one by one")
+        print("Code1: Merge lists one by one")
         if not lists:
             return None
         
